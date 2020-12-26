ed8f1b6c0db5118487601a8e4686ac680563fd19
zhulh <zhulh@chinatelecom.cn>
2020-12-26 13:32:46 +0800

    feat: 设备状态统计

--- src/components/Dashboard/DashboardDevice.vue
+++ src/components/Dashboard/DashboardDevice.vue
@@ -1,31 +1,167 @@
 <template>
   <DashboardContainer title="设备状态统计">
-    <template v-slot:header>
-      最近12小时
-    </template>
-    hsdjfhdsjf
+    <div class="device-stats">
+      <div class="device-stats__chart">
+        <div ref="chart" />
+      </div>
+      <div class="device-stats__data">
+        <div class="device-stats__data__chip device-stats__data__chip--online"><label>在线</label><span>{{ stats.online }}</span></div>
+        <div class="device-stats__data__chip device-stats__data__chip--offline"><label>离线</label><span>{{ stats.offline }}</span></div>
+      </div>
+    </div>
   </DashboardContainer>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Mixins } from 'vue-property-decorator'
+import DashboardMixin from './DashboardMixin'
 import DashboardContainer from './DashboardContainer.vue'
+import { Chart } from '@antv/g2'
 import { getDeviceStates } from '@/api/dashboard'
 
 @Component({
   name: 'DashboardDevice',
   components: { DashboardContainer }
 })
-export default class extends Vue {
+export default class extends Mixins(DashboardMixin) {
+  private stats: any = {}
+
   private mounted() {
     this.getDeviceStates()
   }
 
+  /**
+   * 获取数据
+   */
   private async getDeviceStates() {
-    await getDeviceStates(null)
+    this.stats = await getDeviceStates(null)
+    this.drawChart()
+  }
+
+  /**
+   * 绘制图表
+   */
+  private async drawChart() {
+    const $container: any = this.$refs.chart
+    const data = [
+      { item: '在线', count: parseInt(this.stats.online) },
+      { item: '离线', count: parseInt(this.stats.offline) }
+    ]
+
+    const chart = new Chart({
+      container: $container,
+      autoFit: true,
+      height: 180
+    })
+
+    chart.coordinate('theta', {
+      radius: 1,
+      innerRadius: 0.8
+    })
+
+    chart
+      .data(data)
+      .legend(false)
+
+    chart
+      .interval()
+      .position('count')
+      .color('item', ['#7CC96F', '#E56161'])
+
+    chart.annotation().text({
+      position: [ '50%', '50%' ],
+      content: '设备总数',
+      style: {
+        fontSize: 16,
+        fill: '#98CFFF',
+        fontWeight: 300,
+        textAlign: 'center'
+      },
+      offsetY: -30
+    })
+
+    chart.annotation().text({
+      position: [ '50%', '50%' ],
+      content: this.stats.sum,
+      style: {
+        fontSize: 24,
+        fill: '#fff',
+        fontWeight: 300,
+        textAlign: 'center'
+      }
+    })
+
+    chart.annotation().text({
+      position: [ '50%', '50%' ],
+      content: (parseInt(this.stats.online) / parseInt(this.stats.sum) * 100).toFixed(0) + '%',
+      style: {
+        fontSize: 22,
+        fill: '#7CC96F',
+        fontWeight: 300,
+        textAlign: 'center'
+      },
+      offsetY: 30
+    })
+
+    chart.render()
   }
 }
 </script>
 <style lang="scss" scoped>
+  .device-stats {
+    display: flex;
+    &__chart {
+      flex: 3;
+    }
+    &__data {
+      margin-left: 2rem;
+      flex: 4;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      &__chip {
+        display: flex;
+        border: 1px solid #fff;
+        padding: 20px 0;
+        position: relative;
+        overflow: hidden;
+        &::before {
+          content: ' ';
+          width: 20px;
+          height: 20px;
+          background: #fff;
+          display: block;
+          position: absolute;
+          top: -10px;
+          left: -10px;
+          transform: rotate(45deg);
+        }
 
+        label {
+          flex: 1;
+          text-align: center;
+        }
+        span {
+          flex: 1;
+          font-weight: bold;
+        }
+
+        &--online {
+          border-color: $dashboardGreen;
+          color: $dashboardGreen;
+          &::before {
+            background: $dashboardGreen;
+          }
+        }
+
+        &--offline {
+          border-color: $dashboardRed;
+          color: $dashboardRed;
+          &::before {
+            background: $dashboardRed;
+          }
+        }
+      }
+    }
+  }
 </style>
