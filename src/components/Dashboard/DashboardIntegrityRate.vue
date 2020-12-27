ec128dea5b9cc41fcacd7547f30313be5132800a
KyrieXiao <xiaoren@chinatelecom.cn>
2020-12-27 15:31:27 +0800

    feat: intergrityRate

--- src/components/Dashboard/DashboardIntegrityRate.vue
+++ src/components/Dashboard/DashboardIntegrityRate.vue
@@ -1,27 +1,28 @@
 <template>
   <DashboardContainer ref="mainBox" title="录像完整率" class="container">
     <template v-slot:header>
-      <el-select 
+      <el-select
         v-model="selectValue"
-        size="mini"
+        size="small"
         popper-class="dark-select"
-        @change="changeEvent" 
+        @change="changeEvent"
       >
         <el-option
           v-for="item in selectOptions"
           :key="item.value"
           :label="item.label"
-          :value="item.value">
+          :value="item.value"
+        >
         </el-option>
       </el-select>
     </template>
     <div v-if="selectValue === '0'" class="content">
       <div class="content__calendar">
-        <el-tooltip placement="top" v-for="(item, index) in data" :key="index">
-          <div slot="content">{{item.time}}<br/>{{`rate: ${parseFloat(item.rate * 100).toFixed(1)}%`}}</div>
-          <div class="content__calendar__item" >
+        <el-tooltip v-for="(item, index) in data" :key="index" placement="top" effect="light">
+          <div slot="content">{{ item.time }}<br/>{{ `rate: ${parseFloat(item.rate * 100).toFixed(1)}%` }}</div>
+          <div class="content__calendar__item">
             <p :style="item.itemBgColor">
-              <span>{{item.day === 30 ? '今': item.day}}</span>
+              <span>{{ index + 1 === data.length ? '今': item.day }}</span>
             </p>
           </div>
         </el-tooltip>
@@ -35,12 +36,10 @@
   </DashboardContainer>
 </template>
 
-<script lang="ts"> 
+<script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import DashboardContainer from './DashboardContainer.vue'
-import { Chart } from '@antv/g2';
-import { log } from 'console';
-import { watch } from 'fs';
+import { Chart } from '@antv/g2'
 import { getIntegrityRate } from '@/api/dashboard'
 import { dateFormatInTable } from '@/utils/date'
 
@@ -50,13 +49,14 @@ import { dateFormatInTable } from '@/utils/date'
 })
 export default class extends Vue {
   private data: any = []
+  private data_hours: any = []
   private selectValue: string = '0'
   private selectOptions: any = [{
     value: '0',
     label: '最近30天'
   }, {
     value: '1',
-    label: '表格'
+    label: '最近24小时'
   }]
   private resizeObserver: any
   private chart: any
@@ -66,7 +66,6 @@ export default class extends Vue {
       this.$nextTick(() => {
         this.setCalender()
       })
-      
     } else if (val === '1') {
       this.$nextTick(() => {
         // @ts-ignore
@@ -77,101 +76,129 @@ export default class extends Vue {
   }
 
   mounted() {
+    this.setCalender()
+  }
+  private setCalender() {
     const time = new Date().getTime()
-    var startTime = dateFormatInTable( '', '', time - 3600 * 24 * 30 * 1000)
-    var endTime = dateFormatInTable( '', '', time)
+    var startTime = dateFormatInTable('', '', time - 3600 * 30 * 24 * 1000)
+    var endTime = dateFormatInTable('', '', time)
     getIntegrityRate({
       startTime: startTime,
       endTime: endTime
-    }).then((data1) => {
-      console.log(data1);
-      console.log(startTime, endTime);
-      
+    }).then((res) => {
       this.data = []
-      for (let i=0; i<30; i++) {
+      for (let i = 0; i < 30; i++) {
+        var mock_time = dateFormatInTable('', '', time - 3600 * (29 - i) * 24 * 1000)
         this.data.push({
-          time: "2020年12月15日",
-          day: i+1,
-          rate: i/30,
+          time: mock_time.split(' ')[0],
+          rate: 0,
+          day: parseInt(mock_time.split(' ')[0].split('-')[2])
         })
       }
-      this.setCalender()
-      this.setChart()
-    })
-    
-  }
-  private setCalender() {
-    //日历
-    var gradient = {
-      rgb_top: [124, 201, 111],
-      rgb_bottom: [226, 97, 95]
-    }
-    var rgb = [
-      gradient.rgb_top[0] - gradient.rgb_bottom[0],
-      gradient.rgb_top[1] - gradient.rgb_bottom[1],
-      gradient.rgb_top[2] - gradient.rgb_bottom[2],
-    ]
-    var content = document.getElementsByClassName('content__calendar')[0]
-    for (let i=0; i<30; i++) {
-      var rgb_temp = [
-        gradient.rgb_bottom[0] + rgb[0] * this.data[i].rate,
-        gradient.rgb_bottom[1] + rgb[1] * this.data[i].rate,
-        gradient.rgb_bottom[2] + rgb[2] * this.data[i].rate
+      Object.keys(res.rate).forEach((key) => {
+        var index = this.data.findIndex((item: any) => {
+          return item.time === key.split(' ')[0]
+        })
+        this.data[index] = {
+          time: key.split(' ')[0],
+          rate: res.rate[key],
+          day: parseInt(key.split(' ')[0].split('-')[2])
+        }
+      })
+      // 日历
+      var gradient = {
+        rgb_top: [124, 201, 111],
+        rgb_bottom: [226, 97, 95]
+      }
+      var rgb = [
+        gradient.rgb_top[0] - gradient.rgb_bottom[0],
+        gradient.rgb_top[1] - gradient.rgb_bottom[1],
+        gradient.rgb_top[2] - gradient.rgb_bottom[2]
       ]
-      this.data[i].itemBgColor = `background-color: rgb(${rgb_temp[0]},${rgb_temp[1]},${rgb_temp[2]})`
-    }
+      for (let i = 0; i < this.data.length; i++) {
+        var rgb_temp = [
+          gradient.rgb_bottom[0] + rgb[0] * this.data[i].rate,
+          gradient.rgb_bottom[1] + rgb[1] * this.data[i].rate,
+          gradient.rgb_bottom[2] + rgb[2] * this.data[i].rate
+        ]
+        this.data[i].itemBgColor = `background-color: rgb(${rgb_temp[0]},${rgb_temp[1]},${rgb_temp[2]})`
+      }
+    })
   }
 
   private setChart() {
-    //柱状图
-    // var container = document.getElementsByClassName('container')[0]
-    // var cHeight = container.clientHeight
-    // var cWidth = container.clientWidth
-    var chartData = []
-    for (let i=0; i<this.data.length; i++) {
-      chartData.push({
-        day: this.data[i].day,
-        rate: this.data[i].rate.toFixed(2),
+    const time = new Date().getTime()
+    var startTime = dateFormatInTable('', '', time - 3600 * 23 * 1000)
+    var endTime = dateFormatInTable('', '', time)
+    getIntegrityRate({
+      startTime: startTime,
+      endTime: endTime
+    }).then((res) => {
+      this.data_hours = []
+      Object.keys(res.rate).forEach((key) => {
+        this.data_hours.push({
+          time: key.split(' ')[1].slice(0, -3),
+          rate: res.rate[key]
+        })
       })
-    }
-    this.chart && this.chart.clear()
-    this.chart = new Chart({
-      container: 'chartContainer',
-      // width: 0.9 * cWidth,
-      // height: 0.7 * cHeight,
-      autoFit: true,
-    })
-    this.chart.axis('rate', {
-      label: {
-        formatter: (val: number) => {
-          return +val * 100 + '%';
+      // 柱状图
+      var chartData = []
+      for (let i = 0; i < this.data_hours.length; i++) {
+        chartData.push({
+          time: this.data_hours[i].time,
+          value: this.data_hours[i].rate
+        })
+      }
+      this.chart = new Chart({
+        container: 'chartContainer',
+        autoFit: true
+      })
+      this.chart.scale({
+        time: {
+          range: [0, 1]
         },
-      },
-      grid: null
-    });
-    this.chart.legend(false)
-    this.chart.data(chartData)
-    this.chart
-      .interval()
-      .position('day*rate')
-      .color('rate', (val: number) => {
-        var gradient = {
-          rgb_top: [124, 201, 111],
-          rgb_bottom: [226, 97, 95]
+        value: {
+          alias: 'rate',
+          nice: true,
+          formatter: (val: number) => {
+            return (val * 100).toFixed(0) + '%'
+          }
         }
-        var rgb = [
-          gradient.rgb_top[0] - gradient.rgb_bottom[0],
-          gradient.rgb_top[1] - gradient.rgb_bottom[1],
-          gradient.rgb_top[2] - gradient.rgb_bottom[2],
-        ]
-        var rgb_temp = [
-          gradient.rgb_bottom[0] + rgb[0] * val,
-          gradient.rgb_bottom[1] + rgb[1] * val,
-          gradient.rgb_bottom[2] + rgb[2] * val
-        ]
-        return `rgb(${rgb_temp[0]}, ${rgb_temp[1]}, ${rgb_temp[2]})`
       })
-    this.chart.render()
+      this.chart.legend(false)
+      this.chart.data(chartData)
+      this.chart
+        .interval()
+        .position('time*value')
+        .color('value', (val: number) => {
+          var gradient = {
+            rgb_top: [124, 201, 111],
+            rgb_bottom: [226, 97, 95]
+          }
+          var rgb = [
+            gradient.rgb_top[0] - gradient.rgb_bottom[0],
+            gradient.rgb_top[1] - gradient.rgb_bottom[1],
+            gradient.rgb_top[2] - gradient.rgb_bottom[2]
+          ]
+          var rgb_temp = [
+            gradient.rgb_bottom[0] + rgb[0] * val,
+            gradient.rgb_bottom[1] + rgb[1] * val,
+            gradient.rgb_bottom[2] + rgb[2] * val
+          ]
+          return `rgb(${rgb_temp[0]}, ${rgb_temp[1]}, ${rgb_temp[2]})`
+        })
+      this.chart.axis('value', {
+        grid: null
+      })
+      this.chart.axis('time', {
+        label: {
+          formatter: (val: string) => {
+            return val
+          }
+        }
+      })
+      this.chart.render()
+    })
   }
 }
 </script>
@@ -198,18 +225,18 @@ export default class extends Vue {
       &__item {
       display: flex;
       height: 33%;
-      width: 10%; 
+      width: 10%;
       justify-content: center;
       align-items: center;
       p {
-        font-size: 1rem;
+        font-size: 1em;
         display: flex;
         align-items: center;
         justify-content: center;
         background: #7CC96F;
         border-radius: 100%;
-        width: 2rem;
-        height: 2rem;
+        width: 2em;
+        height: 2em;
         color: white;
         span {
           -webkit-user-select:none;
@@ -221,8 +248,6 @@ export default class extends Vue {
     }
     }
 
-    
-
     &__process {
       position: relative;
       top: 20%;
@@ -233,7 +258,7 @@ export default class extends Vue {
       border-radius: 10px;
 
       &__span {
-        font-size: 0.5rem;
+        font-size: 0.5em;
         position: absolute;
         left: 200%;
 
