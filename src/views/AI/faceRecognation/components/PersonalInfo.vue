3aea62a87e94ead6512896fcafac732f9d225e0f
avivadepp <avivadepp@163.com>
2020-12-27 14:46:51 +0800

    人员添加界面

--- src/views/AI/faceRecognation/components/PersonalInfo.vue
+++ src/views/AI/faceRecognation/components/PersonalInfo.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <el-button type="primary" @click="showAddPesronDialog=true">添加人员</el-button>
-    <el-table v-loading="loading" class="personal-info__table" :data="dataList" fit @row-click="rowClick">
-      <el-table-column prop="profile" label="头像" min-width="200">
+    <el-table v-loading="loading" class="personal-info__table" :data="dataList" fit>
+      <el-table-column prop="profile" label="头像">
         <template slot-scope="{row}">
           {{ row.profile }}
         </template>
@@ -10,23 +10,30 @@
       <el-table-column prop="name" label="姓名" />
       <el-table-column prop="description" label="描述" />
     </el-table>
+    <AddPersonal v-if="showAddPesronDialog" @on-close="showAddPesronDialog=false" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import AddPersonal from './dialogs/AddPersonal'
 
 @Component({
-  name: 'PersonalInfo'
+  name: 'PersonalInfo',
+  components: {
+    AddPersonal
+  }
 })
 export default class extends Vue {
   private showAddPesronDialog = false
   private loading = false
+  private dataList = []
 }
 </script>
 
 <style lang="scss" scoped>
 .personal-info__table {
+  margin-top: 10px;
   ::v-deep .el-table__body {
     td {
       cursor: pointer;
