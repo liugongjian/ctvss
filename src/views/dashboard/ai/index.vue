835619de7fee5968d6786bcdf7b490993f293754
zhulh <zhulh@chinatelecom.cn>
2021-02-20 08:57:19 +0800

    fix: AI页面数据为空报错

--- src/views/dashboard/ai/index.vue
+++ src/views/dashboard/ai/index.vue
@@ -29,8 +29,8 @@
                   {{ location.label }}
                 </div> -->
               </div>
-              <div v-if="type === '2'" class="ai-recognation__images__item__count" :class="{'ai-recognation__images__item__count--warning': currentImg && currentImg.locations && currentImg.locations.length > 10}">聚集人数: {{ currentImg && currentImg.locations && currentImg.locations.length || '-' }}</div>
-              <div class="ai-recognation__images__item--datetime">{{ currentImg.deviceName }} | {{ currentImg && currentImg.timestamp }}</div>
+              <div v-if="type === '2' && currentImg" class="ai-recognation__images__item__count" :class="{'ai-recognation__images__item__count--warning': currentImg && currentImg.locations && currentImg.locations.length > 10}">聚集人数: {{ currentImg && currentImg.locations && currentImg.locations.length || '-' }}</div>
+              <div v-if="currentImg" class="ai-recognation__images__item--datetime">{{ currentImg && currentImg.deviceName }} | {{ currentImg && currentImg.timestamp }}</div>
             </div>
             <player
               v-if="currentVideo"
