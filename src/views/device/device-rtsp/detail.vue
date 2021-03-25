db15a87ffef798292a23cbd968e4e5d64e0dcaf9
zhulh <zhulh@chinatelecom.cn>
2021-03-25 15:16:05 +0800

    feat: 优化设备管理

--- src/views/device/device-rtsp/detail.vue
+++ src/views/device/device-rtsp/detail.vue
@@ -40,13 +40,13 @@
                   </div>
                 </div>
               </info-list-item>
-              <info-list-item v-if="info.inType === 'push'" label="推流地址">
+              <info-list-item v-if="info.inType === 'push'" label="推流地址:">
                 {{ info.pushUrl || '-' }}
                 <el-tooltip v-if="info.pushUrl" class="item" effect="dark" content="复制链接" placement="top">
                   <el-button type="text" @click="copyUrl(info.pushUrl)"><svg-icon name="copy" /></el-button>
                 </el-tooltip>
               </info-list-item>
-              <info-list-item v-else label="拉流地址">
+              <info-list-item v-else label="拉流地址:">
                 {{ info.pullUrl || '-' }}
                 <el-tooltip v-if="info.pullUrl" class="item" effect="dark" content="复制链接" placement="top">
                   <el-button type="text" @click="copyUrl(info.pullUrl)"><svg-icon name="copy" /></el-button>
