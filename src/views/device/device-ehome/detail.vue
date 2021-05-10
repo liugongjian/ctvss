49b3203cb2ea6c6d8434e11561b7eabc33614797
KyrieXiao <xiaoren@chinatelecom.cn>
2021-05-10 09:51:20 +0800

    fix: 去除ehome中inType判断

--- src/views/device/device-ehome/detail.vue
+++ src/views/device/device-ehome/detail.vue
@@ -29,7 +29,6 @@
               <info-list-item label="设备IP:">{{ info.deviceIp }}</info-list-item>
               <info-list-item label="设备端口:">{{ info.devicePort }}</info-list-item>
               <info-list-item label="主子码流数量:">{{ info.autoStreamNum }}</info-list-item>
-              <!-- <info-list-item v-if="info.inType === 'push'" label="自动激活推流地址:">{{ pushType[info.pushType] || '-' }}</info-list-item> -->
               <info-list-item label="自动拉流:">{{ pullType[info.pullType] || '-' }}</info-list-item>
               <info-list-item label="设备状态:">
                 <div class="info-list__edit">
@@ -75,12 +74,6 @@
                 </info-list-item>
               </template>
               <info-list-item label="设备描述:">{{ info.description || '-' }}</info-list-item>
-              <!-- <info-list-item v-if="info.inType === 'push'" label="推流地址:">
-                {{ info.pushUrl || '-' }}
-                <el-tooltip v-if="info.pushUrl" class="item" effect="dark" content="复制链接" placement="top">
-                  <el-button type="text" @click="copyUrl(info.pushUrl)"><svg-icon name="copy" /></el-button>
-                </el-tooltip>
-              </info-list-item> -->
               <info-list-item label="拉流地址:">
                 {{ info.pullUrl || '-' }}
                 <el-tooltip v-if="info.pullUrl" class="item" effect="dark" content="复制链接" placement="top">
@@ -89,89 +82,6 @@
               </info-list-item>
             </info-list>
           </div>
-        </el-tab-pane>
-        <el-tab-pane v-if="false" label="推流配置" name="push">
-          <info-list label-width="115" title="鉴权配置" class="auth-config">
-            <el-button v-if="pushConfig.auth" type="text" class="auth-config__edit" @click="openDialog('setAuthConfig')">编辑鉴权KEY</el-button>
-            <info-list-item label="推流鉴权:">
-              <el-switch v-model="pushConfig.auth" />
-              {{ authStatus[pushConfig.auth] }}
-            </info-list-item>
-            <info-list-item label="主KEY:">{{ pushConfig.key }}</info-list-item>
-            <info-list-item label="备KEY:">{{ pushConfig.key2 }}</info-list-item>
-          </info-list>
-          <info-list label-width="115" title="推流地址生成器">
-            <info-list-item label="过期时间:">
-              <el-date-picker
-                v-model="pushExpired"
-                type="datetime"
-                placeholder="选择过期时间"
-              />
-              <el-button type="primary" class="ml10">生成推流地址</el-button>
-            </info-list-item>
-            <div class="address-maker">
-              <info-list label-width="115">
-                <info-list-item label="推流地址:">
-                  rtmp://102715.push.domain.com:3738/vss/237233774?signature=045bfe2107b98f356e459c8b2bd54be4&expired=5EEC5741
-                  <el-tooltip class="item" effect="dark" content="复制链接" placement="top">
-                    <el-button type="text"><i class="el-icon-copy-document" /></el-button>
-                  </el-tooltip>
-                </info-list-item>
-              </info-list>
-            </div>
-          </info-list>
-        </el-tab-pane>
-        <el-tab-pane v-if="false" label="播流配置" name="play">
-          <info-list label-width="115" title="鉴权配置" class="auth-config">
-            <el-button v-if="playConfig.auth" type="text" class="auth-config__edit" @click="openDialog('setAuthConfig')">编辑鉴权KEY</el-button>
-            <info-list-item label="推流鉴权:">
-              <el-switch v-model="playConfig.auth" />
-              {{ authStatus[playConfig.auth] }}
-            </info-list-item>
-            <transition name="fade">
-              <div v-if="playConfig.auth">
-                <info-list-item label="主KEY:">{{ playConfig.key }}</info-list-item>
-                <info-list-item label="备KEY:">{{ playConfig.key2 }}</info-list-item>
-              </div>
-            </transition>
-          </info-list>
-          <info-list label-width="115" title="推流地址生成器">
-            <info-list-item label="过期时间:">
-              <el-date-picker
-                v-model="pushExpired"
-                type="datetime"
-                placeholder="选择过期时间"
-              />
-              <el-button type="primary" class="ml10">生成推流地址</el-button>
-            </info-list-item>
-            <div class="address-maker">
-              <info-list label-width="100">
-                <info-list-item label="RTMP:">
-                  rtmp://102715.push.domain.com:3738/vss/237233774?signature=045bfe2107b98f356e459c8b2bd54be4&expired=5EEC5741
-                  <el-tooltip class="item" effect="dark" content="复制链接" placement="top">
-                    <el-button type="text"><i class="el-icon-copy-document" /></el-button>
-                  </el-tooltip>
-                </info-list-item>
-                <info-list-item label="FLV:">
-                  https://102715.push.domain.com:3738/vss/237233774.flv?signature=045bfe2107b98f356e459c8b2bd54be4&expired=5EEC5741
-                  <el-tooltip class="item" effect="dark" content="复制链接" placement="top">
-                    <el-button type="text"><i class="el-icon-copy-document" /></el-button>
-                  </el-tooltip>
-                </info-list-item>
-                <info-list-item label="HLS:">
-                  https://102715.push.domain.com:3738/vss/237233774.m3u8?signature=045bfe2107b98f356e459c8b2bd54be4&expired=5EEC5741
-                  <el-tooltip class="item" effect="dark" content="复制链接" placement="top">
-                    <el-button type="text"><i class="el-icon-copy-document" /></el-button>
-                  </el-tooltip>
-                </info-list-item>
-              </info-list>
-            </div>
-          </info-list>
-          <info-list label-width="115" title="防盗链配置">
-            <anti-theft-chain type="Referer" :config="playConfig.anti.referer" />
-            <anti-theft-chain type="IP" :config="playConfig.anti.ip" />
-            <anti-theft-chain type="UA" :config="playConfig.anti.ua" />
-          </info-list>
         </el-tab-pane>
         <el-tab-pane label="模板配置" name="template">
           <template-bind v-if="activeName==='template'" :device-id="deviceId" :in-protocol="inProtocol" />
