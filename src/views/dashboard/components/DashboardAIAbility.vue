622a129fb4c8920ef4eb73d9335b4333b43bc79d
zhulh <zhulh@chinatelecom.cn>
2021-07-06 15:31:41 +0800

    feat: 机房设备检测, 铁塔天线检测

--- src/views/dashboard/components/DashboardAIAbility.vue
+++ src/views/dashboard/components/DashboardAIAbility.vue
@@ -3,31 +3,13 @@
     <template slot="header">
       <el-button type="primary" class="dash-btn" @click="$router.push('/dashboard/visualization-dashboard')">可视化大屏</el-button>
     </template>
-    <p class="dashboard-wrap-overview__cell__head">人脸识别</p>
-    <div class="dashboard-wrap-overview__cell__list">
-      <el-button size="medium" type="text" @click="goRouter(6)">未带口罩</el-button>
-      <div class="column-line" />
-      <el-button size="medium" type="text" @click="goRouter(4)">人员布控</el-button>
-    </div>
-    <p class="dashboard-wrap-overview__cell__head">人体识别</p>
-    <div class="dashboard-wrap-overview__cell__list">
-      <el-button size="medium" type="text" @click="goRouter(8)">人员聚集</el-button>
-      <div class="column-line" />
-      <el-button size="medium" type="text" @click="goRouter(5)">吸烟检测</el-button>
-      <div class="column-line" />
-      <el-button size="medium" type="text" @click="goRouter(7)">安全帽反光服检测</el-button>
-      <div class="column-line" />
-      <el-button size="medium" type="text" @click="goRouter(12)">人体属性</el-button>
-    </div>
-    <p class="dashboard-wrap-overview__cell__head">场景识别</p>
-    <div class="dashboard-wrap-overview__cell__list">
-      <el-button size="medium" type="text" @click="goRouter(9)">危险区域检测</el-button>
-      <div class="column-line" />
-      <el-button size="medium" type="text" @click="goRouter(10)">烟雾明火</el-button>
-      <div class="column-line" />
-      <el-button size="medium" type="text" @click="goRouter(11)">冲压机</el-button>
-      <div class="column-line" />
-      <el-button size="medium" type="text" @click="goRouter(13)">蜜蜂密度</el-button>
+    <div v-for="group in aiGroups" :key="group.name">
+      <p class="dashboard-wrap-overview__cell__head">{{ group.name }}</p>
+      <div class="dashboard-wrap-overview__cell__list">
+        <span v-for="aiType in group.children" :key="aiType">
+          <el-button size="medium" type="text" @click="goRouter(aiType)">{{ alertType[aiType] }}</el-button>
+        </span>
+      </div>
     </div>
   </component>
 </template>
@@ -35,6 +17,8 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import DashboardLightContainer from './DashboardLightContainer.vue'
+import { AlertType } from '@/dics'
+import { AiGroups } from '../helper/aiGroups'
 
 @Component({
   name: 'DashboardAIAbility',
@@ -43,6 +27,9 @@ import DashboardLightContainer from './DashboardLightContainer.vue'
   }
 })
 export default class extends Vue {
+  private alertType = AlertType
+  private aiGroups = AiGroups
+
   private get container() {
     return 'DashboardLightContainer'
   }
@@ -59,3 +46,10 @@ export default class extends Vue {
   }
 }
 </script>
+<style lang="scss" scoped>
+  .dashboard-wrap-overview__cell__list {
+    span {
+      margin-right: 15px;
+    }
+  }
+</style>
