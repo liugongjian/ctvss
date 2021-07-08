3f0e1e980c67d95b84e7a070e00781fb4ecb9dc3
zhulh <zhulh@chinatelecom.cn>
2021-07-08 14:17:18 +0800

    feat: 更新资源包绑定

--- src/views/device/components/ResourceTabs.vue
+++ src/views/device/components/ResourceTabs.vue
@@ -2,26 +2,26 @@
   <el-tabs v-model="resourceTabType" type="card" class="resource-tabs">
     <el-tab-pane label="视频包" name="video">
       <!--视频包-->
-      <div class="resource-tabs__content">
+      <div v-loading="loading.resouceVideoList" class="resource-tabs__content">
         <el-table :data="resouceVideoList" @row-click="onResourceRowClick('video', ...arguments)">
-          <el-table-column prop="resourceId" label="编号">
+          <el-table-column prop="id" label="编号">
             <template scope="scope">
-              <el-radio v-model="form.resouceVideo" :label="scope.row.resourceId" />
+              <el-radio v-model="form.resouceVideo" :label="scope.row.id" />
             </template>
           </el-table-column>
-          <el-table-column prop="value" label="可接入设备数量">
+          <el-table-column prop="totalDeviceCount" label="可接入设备数量">
             <template scope="scope">
-              {{ scope.row.value }}路
+              {{ scope.row.totalDeviceCount }}路
             </template>
           </el-table-column>
-          <el-table-column prop="value" label="接入设备余量">
+          <el-table-column prop="remainDeviceCount" label="接入设备余量">
             <template scope="scope">
-              {{ scope.row.value }}路
+              {{ scope.row.remainDeviceCount }}路
             </template>
           </el-table-column>
-          <el-table-column prop="codeRate" label="码率">
+          <el-table-column prop="bitRate" label="码率">
             <template scope="scope">
-              {{ scope.row.codeRate }}Mbps
+              {{ scope.row.bitRate }}Mbps
             </template>
           </el-table-column>
           <el-table-column prop="storageTime" label="存储周期">
@@ -29,31 +29,31 @@
               {{ scope.row.storageTime }}天
             </template>
           </el-table-column>
-          <el-table-column prop="startTime" label="开通时间" />
-          <el-table-column prop="endTime" label="到期时间" />
+          <el-table-column prop="createTime" label="开通时间" />
+          <el-table-column prop="expireTime" label="到期时间" />
         </el-table>
-        <div class="resource-tabs__none">
+        <div v-if="resouceVideoList.length" class="resource-tabs__none">
           <el-radio v-model="form.resouceVideo" :label="-1" @change="onFormChange">不绑定任何视频包</el-radio>
         </div>
       </div>
     </el-tab-pane>
     <el-tab-pane label="AI包" name="ai">
       <!--AI包-->
-      <div class="resource-tabs__content">
+      <div v-loading="loading.resouceAiList" class="resource-tabs__content">
         <el-table :data="resouceAiList" @row-click="onResourceRowClick('ai', ...arguments)">
-          <el-table-column prop="resourceId" label="编号">
+          <el-table-column prop="id" label="编号">
             <template scope="scope">
-              <el-radio v-model="form.resouceAi" :label="scope.row.resourceId" />
+              <el-radio v-model="form.resouceAi" :label="scope.row.id" />
             </template>
           </el-table-column>
-          <el-table-column prop="value" label="可接入设备数量">
+          <el-table-column prop="totalDeviceCount" label="可接入设备数量">
             <template scope="scope">
-              {{ scope.row.value }}路
+              {{ scope.row.totalDeviceCount }}路
             </template>
           </el-table-column>
-          <el-table-column prop="value" label="接入设备余量">
+          <el-table-column prop="remainDeviceCount" label="接入设备余量">
             <template scope="scope">
-              {{ scope.row.value }}路
+              {{ scope.row.remainDeviceCount }}路
             </template>
           </el-table-column>
           <el-table-column prop="aiType" label="分析类型">
@@ -61,21 +61,21 @@
               {{ resourceAiType[scope.row.aiType] }}
             </template>
           </el-table-column>
-          <el-table-column prop="startTime" label="开通时间" />
-          <el-table-column prop="endTime" label="到期时间" />
+          <el-table-column prop="createTime" label="开通时间" />
+          <el-table-column prop="expireTime" label="到期时间" />
         </el-table>
-        <div class="resource-tabs__none">
+        <div v-if="resouceAiList.length" class="resource-tabs__none">
           <el-radio v-model="form.resouceAi" :label="-1" @change="onFormChange">不绑定任何视频包</el-radio>
         </div>
       </div>
     </el-tab-pane>
     <el-tab-pane label="上行带宽包" name="upload">
       <!--上行带宽包-->
-      <div class="resource-tabs__content">
+      <div v-loading="loading.resouceUploadList" class="resource-tabs__content">
         <el-table :data="resouceUploadList" @row-click="onResourceRowClick('upload', ...arguments)">
-          <el-table-column prop="resourceId" label="编号">
+          <el-table-column prop="id" label="编号">
             <template scope="scope">
-              <el-radio v-model="form.resouceUpload" :label="scope.row.resourceId" />
+              <el-radio v-model="form.resouceUpload" :label="scope.row.id" />
             </template>
           </el-table-column>
           <el-table-column prop="value" label="剩余上行带宽">
@@ -83,10 +83,10 @@
               {{ scope.row.value }}Mbps
             </template>
           </el-table-column>
-          <el-table-column prop="startTime" label="开通时间" />
-          <el-table-column prop="endTime" label="到期时间" />
+          <el-table-column prop="createTime" label="开通时间" />
+          <el-table-column prop="expireTime" label="到期时间" />
         </el-table>
-        <div class="resource-tabs__none">
+        <div v-if="resouceUploadList.length" class="resource-tabs__none">
           <el-radio v-model="form.resouceUpload" :label="-1" @change="onFormChange">不绑定任何视频包</el-radio>
         </div>
       </div>
@@ -96,7 +96,7 @@
 <script lang='ts'>
 import { Component, Prop, Watch, Vue } from 'vue-property-decorator'
 import { ResourceAiType } from '@/dics'
-// import { Device } from '@/type/device'
+import { getResources } from '@/api/billing'
 
 @Component({
   name: 'ResourceTabs'
@@ -111,54 +111,38 @@ export default class extends Vue {
     resouceAi: null,
     resouceUpload: null
   }
-  private resouceVideoList = [
-    {
-      resourceId: 1,
-      value: 100,
-      codeRate: 8,
-      storageTime: 7,
-      startTime: '2021-04-29 11:03:32',
-      endTime: '2021-04-29 11:03:32'
-    },
-    {
-      resourceId: 2,
-      value: 100,
-      codeRate: 8,
-      storageTime: 7,
-      startTime: '2021-04-29 11:03:32',
-      endTime: '2021-04-29 11:03:32'
+  private loading = {
+    resouceVideoList: false,
+    resouceAiList: false,
+    resouceUploadList: false
+  }
+  private resouceVideoList = []
+  private resouceAiList = []
+  private resouceUploadList = []
+
+  private async mounted() {
+    this.resouceVideoList = await this.getResouces('VSS_VIDEO', this.loading.resouceVideoList)
+    this.resouceAiList = await this.getResouces('VSS_AI', this.loading.resouceAiList)
+    this.resouceUploadList = await this.getResouces('VSS_UPLOAD_BW', this.loading.resouceUploadList)
+  }
+
+  /**
+   * 加载资源包
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  private async getResouces(type: string, loading: any) {
+    try {
+      loading = true
+      const res = await getResources({
+        type
+      })
+      return res.resPkgList
+    } catch (e) {
+      this.$message.error(e.message)
+    } finally {
+      loading = false
     }
-  ]
-  private resouceAiList = [
-    {
-      resourceId: 1,
-      value: 100,
-      aiType: 'AI-100',
-      startTime: '2021-04-29 11:03:32',
-      endTime: '2021-04-29 11:03:32'
-    },
-    {
-      resourceId: 2,
-      value: 100,
-      aiType: 'AI-100',
-      startTime: '2021-04-29 11:03:32',
-      endTime: '2021-04-29 11:03:32'
-    }
-  ]
-  private resouceUploadList = [
-    {
-      resourceId: 1,
-      value: 100,
-      startTime: '2021-04-29 11:03:32',
-      endTime: '2021-04-29 11:03:32'
-    },
-    {
-      resourceId: 2,
-      value: 100,
-      startTime: '2021-04-29 11:03:32',
-      endTime: '2021-04-29 11:03:32'
-    }
-  ]
+  }
 
   private onFormChange() {
     this.$emit('resource-change', this.form)
