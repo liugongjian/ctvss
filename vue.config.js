86a0644eefaa641487a491ad1bf417adf2fdcbfa
KyrieXiao <xiaoren@chinatelecom.cn>
2021-08-02 13:43:50 +0800

    fix: config.vue

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,7 @@ const environment = process.argv[3] === '--env' ? process.argv[4] : 'dev'
 const isHttps = process.argv[process.argv.length - 1] === '--https'
 const name = '天翼云视频云网平台-客户控制台'
 const serverAddressMapping = {
-<<<<<<< HEAD
-  local: 'http://192.168.30.48:8081/', // 本地环境
-=======
   local: 'http://192.168.30.170:8081', // 本地环境
->>>>>>> develop
   dev: 'http://182.43.127.35:9190', // 开发环境
   test: 'https://182.43.127.35:9180', // 测试环境
   prod: 'https://182.43.127.45' // 生产环境
