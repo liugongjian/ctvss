a8fdd5273d2f1af70e665b569e9c3b06ec87f739
zhulh <zhulh@chinatelecom.cn>
2021-10-09 14:30:35 +0800

    feat: arrow-right icon

---
+++ src/icons/components/arrow-right.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'arrow-right': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 109 187',
+    data: '<path pid="0" d="M100.9 85.4L21.3 5.8c-4.4-4.4-11.5-4.4-15.9 0S1 17.3 5.4 21.7l71.7 71.7-71.7 71.7C1 169.5 1 176.6 5.4 181s11.5 4.4 15.9 0l79.6-79.7c4.5-4.4 4.5-11.5 0-15.9z" _fill="#2c2c2c"/>'
+  }
+})
