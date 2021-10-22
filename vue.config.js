1b86e9349229e9f78735d46dca7f8f2501f28ccf
rejiu <zhaodan@chinatelecom.cn>
2021-10-22 18:07:44 +0800

    修改 ehome子通道配置 子设备数量最小不可少于已配置数

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,8 @@ const serverAddressMapping = {
   dev2: 'http://182.43.127.35:9050', // 开发环境2
   test: 'https://182.43.127.35:9180', // 测试环境
   test2: 'https://182.43.127.35:9160', // 测试环境2
-  prod: 'https://182.43.127.45' // 生产环境
+  prod: 'https://182.43.127.45', // 生产环境
+  testTemp: 'http://182.43.127.35:9080' // 测试环境
 }
 const portMapping = {
   local: 9180,
@@ -16,7 +17,8 @@ const portMapping = {
   dev2: 9050,
   test: 9180,
   test2: 9160,
-  prod: 443
+  prod: 443,
+  testTemp: 9080
 }
 const serverAddress = serverAddressMapping[environment]
 const devServerPort = portMapping[environment]
