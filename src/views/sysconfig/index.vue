805902cffa4ac6e31cd38d074a72b079b43b971e
KyrieXiao <xiaoren@chinatelecom.cn>
2022-02-09 13:56:26 +0800

    fix: 整合短信配置与观看记录保存配置

--- src/views/sysconfig/index.vue
+++ src/views/sysconfig/index.vue
@@ -21,6 +21,38 @@
         <el-form-item v-if="form.active" label="手机号:" prop="phoneNumber">
           <el-input v-model="form.phoneNumber" />
         </el-form-item>
+        <el-form-item prop="screen">
+          <template slot="label">
+            实时预览记录功能:
+            <el-popover
+              placement="top-start"
+              title="实时预览记录功能"
+              width="400"
+              trigger="hover"
+              :open-delay="300"
+              :content="tips.screen"
+            >
+              <svg-icon slot="reference" class="form-question" name="help" />
+            </el-popover>
+          </template>
+          <el-switch v-model="form.screen" active-value="true" inactive-value="false" />
+        </el-form-item>
+        <el-form-item prop="replay">
+          <template slot="label">
+            录像回看记录功能:
+            <el-popover
+              placement="top-start"
+              title="录像回看记录功能"
+              width="400"
+              trigger="hover"
+              :open-delay="300"
+              :content="tips.replay"
+            >
+              <svg-icon slot="reference" class="form-question" name="help" />
+            </el-popover>
+          </template>
+          <el-switch v-model="form.replay" active-value="true" inactive-value="false" />
+        </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="submit">提交</el-button>
           <el-button @click="back">取消</el-button>
@@ -33,6 +65,8 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import { getPhoneNumberForAISMS, activatePhone } from '@/api/ai-app'
+import { UserModule } from '@/store/modules/user'
+import { updatetUserConfig } from '@/api/users'
 
 function isvalidPhone(str) {
   const reg = /^1[3|4|5|7|8][0-9]\d{8}$/
@@ -53,13 +87,43 @@ const validPhone = (rule, value, callback) => {
 export default class extends Vue {
   public form:any = {
     active: false,
-    phoneNumber: ''
+    phoneNumber: '',
+    screen: 'false',
+    replay: 'false'
   }
   public rules = {
     phoneNumber: [{ required: true, trigger: 'blur', validator: validPhone }]
   }
+
+  private tips = {
+    screen: '除首次实时预览需要打开指定摄像头外，后续切换回实时预览模块，都会直接播放上一次摄像头实时画面，默认关闭',
+    replay: '除首次录像回放需要打开指定摄像头外，后续切换回录像回放模块，都会自动打开上一次摄像头录像回放界面，默认关闭'
+  }
+  private cacheForm = {
+    screen: 'false',
+    replay: 'false'
+  }
+  private loading: boolean = false
+
+  private get screenCacheSettings() {
+    return UserModule.settings.screenCache
+  }
+
   private async mounted() {
-    this.form = await getPhoneNumberForAISMS({})
+    try {
+      this.form = await getPhoneNumberForAISMS({})
+    } catch (e) {
+      console.log(e)
+    }
+    try {
+      await UserModule.getUserConfigInfo()
+      this.form = {
+        ...this.form,
+        ...this.screenCacheSettings
+      }
+    } catch (e) {
+      console.log(e)
+    }
   }
   private back() {
     this.$router.back()
@@ -69,6 +133,24 @@ export default class extends Vue {
     form.validate(async(valid: any) => {
       if (!valid) return
       try {
+        this.loading = true
+        let params = []
+        // 前后端参数不一致，设置转换字典
+        let dic = {
+          screen: 'live',
+          replay: 'record'
+        }
+        let keyList = ['screen', 'replay']
+        keyList.forEach(item => {
+          params.push({
+            type: dic[item],
+            enable: this.form[item]
+          })
+        })
+        await updatetUserConfig({
+          userConfig: params
+        })
+        await UserModule.getUserConfigInfo()
         await activatePhone({
           phoneNumber: this.form.phoneNumber,
           active: this.form.active
@@ -78,9 +160,37 @@ export default class extends Vue {
         this.$message.error('操作失败')
       } finally {
         this.form = await getPhoneNumberForAISMS({})
+        this.loading = false
       }
     })
   }
+
+  // private async handleChangeSettings() {
+  //   try {
+  //     this.loading = true
+  //     let params = []
+  //     // 前后端参数不一致，设置转换字典
+  //     let dic = {
+  //       screen: 'live',
+  //       replay: 'record'
+  //     }
+  //     Object.keys(this.form).forEach(item => {
+  //       params.push({
+  //         type: dic[item],
+  //         enable: this.form[item]
+  //       })
+  //     })
+  //     await updatetUserConfig({
+  //       userConfig: params
+  //     })
+  //     await UserModule.getUserConfigInfo()
+  //     this.$message.success('修改用户配置成功！')
+  //   } catch (err) {
+  //     this.$message.error('修改用户配置失败，失败原因：' + err.message)
+  //   } finally {
+  //     this.loading = false
+  //   }
+  // }
 }
 
 </script>
