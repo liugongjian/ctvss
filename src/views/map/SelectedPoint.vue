d6256e7e25b0495c59428d434137982e5dd396c2
chenc_1 <9869087+chenchengakki@user.noreply.gitee.com>
2022-03-22 19:36:48 +0800

    feat: 添加地图和地图信息功能修改和添加

---
+++ src/views/map/SelectedPoint.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-descriptions title="共选中两个节点" :column="1">
+      <el-descriptions-item label="IPC1">
+        <el-input v-model="editValue" disabled />
+      </el-descriptions-item>
+      <el-descriptions-item label="IPC2">
+        <el-input v-model="editValue" disabled />
+      </el-descriptions-item>
+    </el-descriptions>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
