ba6c8eadac8174d459611663350f4ab9840e054f
zhulh <zhulh@chinatelecom.cn>
2022-03-23 13:47:46 +0800

    feat: 时间轴disable状态

--- src/views/device/components/ScreenBoard/components/Sync.vue
+++ src/views/device/components/ScreenBoard/components/Sync.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="screen-tools__btn" :class="{'selected': isSync}" @click="toggleSync">
-    <svg-icon name="arrow-right" />
+    <el-tooltip v-if="isSync" content="当前为同步回放模式" placement="top">
+      <svg-icon class="sync" name="sync" width="16" height="16" />
+    </el-tooltip>
+    <el-tooltip v-else content="当前为异步回放模式" placement="top">
+      <svg-icon name="async" width="16" height="16" />
+    </el-tooltip>
   </div>
 </template>
 <script lang="ts">
@@ -17,7 +22,12 @@ export default class extends ComponentMixin {
 
   private toggleSync() {
     this.screenManager.isSync = !this.screenManager.isSync
+    this.$message(`已切换为${this.screenManager.isSync ? '同步' : '异步'}回放模式`)
   }
 }
-
 </script>
+<style lang="scss" scoped>
+.sync {
+  color: $primary;
+}
+</style>
