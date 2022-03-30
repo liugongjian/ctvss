80e5a31f55fd5ae7395b61dcde223e48f4703c64
xiaoren <xiaoren@chinatelecom.cn>
2022-03-30 14:14:52 +0800

    feat: 消息推送-推送对象及资源选择树重构

--- src/views/notification/policy/createOrUpdate.vue
+++ src/views/notification/policy/createOrUpdate.vue
@@ -105,7 +105,7 @@
             />
           </el-select>
         </el-form-item>
-        <el-form-item label="消息内容" prop="notifyTemplate">
+        <el-form-item label="消息内容：" prop="notifyTemplate">
           <el-input
             v-model="form.notifyTemplate"
             class="form__input"
@@ -115,92 +115,16 @@
           />
         </el-form-item>
         <el-form-item v-if="form.source !== '2'" label="生效资源：" prop="notifyResources">
-          <div class="dialog-wrap">
-            <div v-loading="loading.dir" class="tree-wrap">
-              <el-tree
-                ref="dirTree"
-                node-key="id"
-                lazy
-                show-checkbox
-                :data="dirList"
-                :load="loadDirs"
-                :props="treeProp"
-                :check-strictly="false"
-                @check-change="onCheckDevice"
-              >
-                <span slot-scope="{node, data}" class="custom-tree-node" :class="`custom-tree-node__${data.type}`">
-                  <span class="node-name">
-                    <svg-icon :name="data.type" color="#6e7c89" />
-                    {{ node.label }}
-                  </span>
-                </span>
-              </el-tree>
-            </div>
-            <div class="device-wrap">
-              <div class="device-wrap__header">已选资源({{ form.resourceList.length }})</div>
-              <el-table ref="deviceTable" :data="form.resourceList" empty-text="暂无选择资源" fit>
-                <el-table-column key="label" prop="label" width="180" label="业务组/目录名称">
-                  <template slot-scope="{row}">
-                    {{ row.label || '-' }}
-                  </template>
-                </el-table-column>
-                <el-table-column key="path" prop="path" label="所在位置">
-                  <template slot-scope="{row}">
-                    {{ renderPath(row.path) }}
-                  </template>
-                </el-table-column>
-                <el-table-column label="操作" prop="action" class-name="col-action" width="110" fixed="right">
-                  <template slot-scope="scope">
-                    <el-button type="text" @click="removeDevice(scope.row)">移除</el-button>
-                  </template>
-                </el-table-column>
-              </el-table>
-            </div>
-          </div>
+          <resource-tree
+            :checkedList="resourceList"
+            @resourceListChange="resourceListChange"
+          />
         </el-form-item>
-        <el-form-item v-if="form.source !== '2'" label="推送对象：" prop="notifyDestinations">
-          <div class="dialog-wrap">
-            <div v-loading="loading.dir" class="tree-wrap">
-              <el-tree
-                ref="dirTree"
-                node-key="id"
-                lazy
-                show-checkbox
-                :data="dirList"
-                :load="loadDirs"
-                :props="treeProp"
-                :check-strictly="false"
-                @check-change="onCheckDevice"
-              >
-                <span slot-scope="{node, data}" class="custom-tree-node" :class="`custom-tree-node__${data.type}`">
-                  <span class="node-name">
-                    <svg-icon :name="data.type" color="#6e7c89" />
-                    {{ node.label }}
-                  </span>
-                </span>
-              </el-tree>
-            </div>
-            <div class="device-wrap">
-              <div class="device-wrap__header">已选资源({{ form.resourceList.length }})</div>
-              <el-table ref="deviceTable" :data="form.resourceList" empty-text="暂无选择对象" fit>
-                <el-table-column key="label" prop="label" width="180" label="对象名称">
-                  <template slot-scope="{row}">
-                    {{ row.label || '-' }}
-                  </template>
-                </el-table-column>
-                <el-table-column key="path" prop="path" label="资源归属">
-                  <template slot-scope="{row}">
-                    {{ renderPath(row.path) }}
-                  </template>
-                </el-table-column>
-                <el-table-column label="操作" prop="action" class-name="col-action" width="110" fixed="right">
-                  <template slot-scope="scope">
-                    <el-button type="text" @click="removeDevice(scope.row)">移除</el-button>
-                  </template>
-                </el-table-column>
-              </el-table>
-            </div>
-          </div>
+        <el-form-item label="推送对象：" prop="notifyDestinations">
+          <destinations-tree
+            :checkedList="destinationList"
+            @destinationListChange="destinationListChange"
+          />
         </el-form-item>
         <el-form-item label="立即生效：" prop="active">
           <el-switch
@@ -230,9 +154,15 @@ import { getGroups } from '@/api/group'
 import { getNotificationPolicyInfo, createNotificationPolicy, editNotificationPolicy } from '@/api/notification'
 import { INotifictionPolicyForm } from '@/type/notification'
 import { dateFormat } from '@/utils/date'
+import ResourceTree from './components/ResourceTree.vue'
+import DestinationsTree from './components/DestinationsTree.vue'
 
 @Component({
-  name: 'notification-policy-create-or-update'
+  name: 'notification-policy-create-or-update',
+  components: {
+    ResourceTree,
+    DestinationsTree
+  }
 })
 export default class extends Vue {
   private breadCrumbContent = ''
@@ -262,7 +192,6 @@ export default class extends Vue {
     active: 1,
     /* 辅助字段 */
     effectiveTimeType: 'all',
-    resourceList: [],
     policyName: '',
     desc: ''
   }
@@ -283,6 +212,9 @@ export default class extends Vue {
     { value: '2', label: '行人检测' },
     { value: '3', label: '口罩检测' }
   ]
+
+  private resourceList = []
+  private destinationList = []
 
   private rules = {
     name: [
@@ -325,7 +257,7 @@ export default class extends Vue {
   }
 
   private validateResourceList(rule: any, value: string, callback: Function) {
-    if (!this.form.resourceList.length) {
+    if (!this.form.notifyResources.length) {
       callback(new Error('资源列表不能为空！'))
     } else {
       callback()
@@ -338,7 +270,6 @@ export default class extends Vue {
 
   private async mounted() {
     this.breadCrumbContent = this.$route.meta.title
-    await this.initDirs()
     if (this.isUpdate) {
       const id = this.$route.query.id
       if (id) {
@@ -396,206 +327,13 @@ export default class extends Vue {
     }
   }
 
-
-  /* 资源选择相关 */
-  /*
-   * 获取策略详情
-   */
-  private async getNotificationPolicyInfo() {
-    try {
-      this.loading.resource = true
-      const res: any = await getNotificationPolicyInfo({
-        id: this.form.id
-      })
-      this.form.policyName = res.policyName
-      this.form.desc = res.desc
-      const policyInfo = JSON.parse(res.policyDocument)
-      const resourceList = policyInfo.Statement[0].Resource
-      this.initResourceStatus(resourceList)
-    } catch (e) {
-      console.log('e: ', e)
-      this.$message.error('查询策略详情出错！')
-    } finally {
-      this.loading.resource = false
-    }
+  private resourceListChange(resourceList) {
+    this.form.notifyResources = resourceList
   }
-
-  /**
-   * 初始化资源选中状态
-   */
-  public async initResourceStatus(resourceList: any) {
-    const dirTree: any = this.$refs.dirTree
-    const checkedKeys = []
-    for (let index = 0, len = resourceList.length; index < len; index++) {
-      const resource = resourceList[index]
-      if (/vssgroup/.test(resource)) {
-        const _key = resource.split(':').slice(-1)[0]
-        checkedKeys.push(_key)
-      } else {
-        const keyPath = resource.split(':').slice(2).join('/').split(/\//)
-        if (keyPath && keyPath.length) {
-          for (let i = 0; i < keyPath.length - 1; i++) {
-            const _key = keyPath[i]
-            const node = dirTree.getNode(_key)
-            if (node) {
-              await this.loadDirChildren(_key, node)
-            }
-          }
-          checkedKeys.push(keyPath[keyPath.length - 1])
-        }
-      }
-    }
-    dirTree.setCheckedKeys(checkedKeys)
+  
+  private destinationListChange(destinationList) {
+    this.form.notifyDestinations = destinationList
   }
-
-  /**
-   * 加载子目录
-   */
-  public async loadDirChildren(key: string, node: any) {
-    if (node.loaded) {
-      node.parent.expanded = true
-      return
-    }
-    try {
-      const dirTree: any = this.$refs.dirTree
-      let data = await getDeviceTree({
-        groupId: node.data.groupId,
-        id: node.data.type === 'group' ? 0 : node.data.id,
-        inProtocol: node.data.inProtocol,
-        type: node.data.type === 'group' ? undefined : node.data.type
-      })
-      if (data.dirs) {
-        const dirs = data.dirs.filter((dir: any) => dir.type === 'dir').map((dir: any) => ({
-          id: dir.id,
-          groupId: node.data.groupId,
-          label: dir.label,
-          inProtocol: node.data.inProtocol,
-          isLeaf: dir.isLeaf,
-          type: dir.type,
-          path: node.data.path.concat([dir]),
-          parentId: node.data.id
-        }))
-        dirTree.updateKeyChildren(key, dirs)
-      }
-      node.expanded = true
-      node.loaded = true
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  /**
-   * 目录初始化
-   */
-  public async initDirs() {
-    try {
-      this.loading.dir = true
-      const res = await getGroups({
-        pageSize: 1000
-      })
-      this.dirList = []
-      res.groups.forEach((group: any) => {
-        (group.inProtocol !== 'vgroup') && (
-          this.dirList.push({
-            id: group.groupId,
-            groupId: group.groupId,
-            label: group.groupName,
-            inProtocol: group.inProtocol,
-            type: 'group',
-            parentId: '0',
-            path: [{
-              id: group.groupId,
-              label: group.groupName,
-              type: 'group'
-            }]
-          })
-        )
-      })
-    } catch (e) {
-      this.dirList = []
-    } finally {
-      this.loading.dir = false
-    }
-  }
-
-  /**
-   * 加载目录
-   */
-  private async loadDirs(node: any, resolve: Function) {
-    if (node.level === 0) return resolve([])
-    const dirs = await this.getTree(node)
-    resolve(dirs)
-  }
-
-  /**
-   * 获取菜单树
-   */
-  private async getTree(node: any) {
-    try {
-      let shareDeviceIds: any = []
-
-      const devices: any = await getDeviceTree({
-        groupId: node.data.groupId,
-        id: node.data.type === 'group' ? 0 : node.data.id,
-        inProtocol: node.data.inProtocol,
-        type: node.data.type === 'group' ? undefined : node.data.type
-      })
-
-      const dirTree: any = this.$refs.dirTree
-      let checkedKeys = dirTree.getCheckedKeys()
-      let dirs: any = devices.dirs.filter((dir: any) => dir.type === 'dir').map((dir: any) => {
-        if (shareDeviceIds.includes(dir.id) && dir.type === 'ipc') {
-          checkedKeys.push(dir.id)
-          dirTree.setCheckedKeys(checkedKeys)
-        }
-        return {
-          id: dir.id,
-          groupId: node.data.groupId,
-          label: dir.label,
-          inProtocol: node.data.inProtocol,
-          isLeaf: dir.isLeaf,
-          type: dir.type,
-          path: node.data.path.concat([dir]),
-          parentId: node.data.id
-        }
-      })
-      return dirs
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  /**
-   * 当设备被选中时回调，将选中的设备列出
-   */
-  private onCheckDevice() {
-    const dirTree: any = this.$refs.dirTree
-    const nodes = dirTree.getCheckedNodes()
-    this.form.resourceList = nodes.filter((node: any) => {
-      const nodeIdsList = nodes.map((node: any) => node.id)
-      return nodeIdsList.indexOf(node.parentId) === -1
-    })
-  }
-
-  /**
-   * 移除设备
-   */
-  private removeDevice(device: any) {
-    const dirTree: any = this.$refs.dirTree
-    dirTree.setChecked(device.id, false)
-  }
-
-  /**
-   * 显示设备所在路径
-   */
-  private renderPath(path: any) {
-    const dirPath = path.slice(0, -1)
-    const dirPathName = dirPath.map((dir: any) => {
-      return dir.label
-    })
-    return dirPathName.join('/')
-  }
-
   
   private back() {
     // this.$router.push(`/accessManage/policy`)
@@ -634,60 +372,6 @@ export default class extends Vue {
 
   .policy {
     display: flex;
-  }
-
-  .list {
-    height: 400px;
-    width: 400px;
-    overflow: auto;
-    border: 1px solid #d7d7d7;
-    border-top: none;
-    border-radius: 5px;
-
-    &__title {
-      height: 40px;
-      line-height: 40px;
-      background: #f2f2f2;
-      padding-left: 20px;
-    }
-  }
-
-  .el-table {
-    width: 100%;
-  }
-
-  .el-table ::v-deep .is-ctyun-policy .cell .el-checkbox__inner {
-    display: none;
-    position: relative;
-  }
-
-  .dialog-wrap {
-    display: flex;
-    border: 1px solid $borderGrey;
-  }
-
-  .tree-wrap {
-    flex: 1 0;
-    height: 550px;
-    padding: 10px;
-    overflow: auto;
-    border-right: 1px solid $borderGrey;
-
-    .is-disabled + .custom-tree-node__ipc {
-      cursor: not-allowed;
-    }
-  }
-
-  .device-wrap {
-    flex: 1 0;
-    height: 550px;
-    overflow: auto;
-
-    &__header {
-      font-weight: bold;
-      text-align: center;
-      padding: 10px;
-    }
   }
 
   .time-table {
