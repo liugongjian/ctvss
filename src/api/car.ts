23ae28ecceb127b9eb4aad17c4c2a58189946602
liugongjian <liugongjianxin@163.com>
2022-05-30 17:22:20 +0800

    'feat:车辆管理列表联调'

---
+++ src/api/car.ts
@@ -0,0 +1,16 @@
+import request from '@/utils/request'
+
+// 车辆管理相关接口
+export const getCarTasks = (params: any): Promise<any> =>
+  request({
+    url: '/vehicle/tasks',
+    method: 'get',
+    params
+  })
+
+export const getCarTask = (params: any): Promise<any> =>
+  request({
+    url: '/vehicle/task',
+    method: 'get',
+    params
+  })
