396bf685a53d1f40dd3fb94fcaf70e44be7f7e64
rejiu <zhaodan@chinatelecom.cn>
2022-06-15 16:10:58 +0800

    电子地图 点位查看静态

--- src/views/map/components/CustomInfo/index.vue
+++ src/views/map/components/CustomInfo/index.vue
@@ -1,16 +1,17 @@
 <template>
-  <div>
-    11
+  <div class="map-point">
+    <base-info />
   </div>
 </template>
 
 <script  lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator'
+import BaseInfo from './BaseInfo.vue'
 
 @Component({
   name: 'CustomInfo',
   components: {
-
+    BaseInfo
   }
 })
 export default class CustomInfo extends Vue {
@@ -18,6 +19,36 @@ export default class CustomInfo extends Vue {
 }
 </script>
 
-<style>
+<style  lang="scss" scoped>
+.map-point {
+  ::v-deep .map-point-base-info {
+    .el-descriptions {
+      font-size: 14px;
+
+      &__header {
+        padding: 20px;
+        border-bottom: 1px solid #d3d3d3;
+      }
+
+      &__title {
+        font-weight: initial;
+      }
+
+      &__body {
+        background: transparent;
+        padding: 0 20px;
+      }
+
+      .el-descriptions-item {
+        &__label {
+          &:not(.is-bordered-label) {
+            width: 60px;
+            text-align: right;
+          }
+        }
+      }
+    }
+  }
+}
 
 </style>
