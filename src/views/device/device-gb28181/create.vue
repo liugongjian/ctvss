677c17ab3cb908dcdfae49dde0cc17239b53434e
rejiu <zhaodan@chinatelecom.cn>
2022-06-23 15:23:38 +0800

    VSS-5541--创建/编辑设备前端改造静态基本完成

--- src/views/device/device-gb28181/create.vue
+++ src/views/device/device-gb28181/create.vue
@@ -97,8 +97,14 @@
               />
             </el-select>
           </el-form-item>
-          <el-form-item label="设备名称:" prop="deviceName" class="form-with-tip">
-            <el-input v-model="form.deviceName" />
+          <el-form-item v-if="isUpdate" :label="ifUseDeviceName ? '设备实际名称:' :'设备名称:' " prop="deviceName" class="form-with-tip">
+            <el-input v-model="form.deviceName" :readonly="ifUseDeviceName" :show-password="ifUseDeviceName" />
+            <div class="form-tip">
+              2-64位，可包含大小写字母、数字、中文、中划线、下划线、小括号、空格。
+            </div>
+          </el-form-item>
+          <el-form-item v-else label="设备名称:" prop="deviceName" class="form-with-tip">
+            <el-input v-model="form.deviceName" readonly />
             <div class="form-tip">
               2-64位，可包含大小写字母、数字、中文、中划线、下划线、小括号、空格。
             </div>
@@ -111,7 +117,7 @@
               @change="onDeviceAddressChange"
             />
           </el-form-item>
-          <el-form-item v-show="form.deviceType !== 'platform'" label="经纬度:" prop="longlat">
+          <el-form-item label="经纬度:" prop="longlat">
             <el-input v-model="form.deviceLongitude" class="longlat-input" /> :
             <el-input v-model="form.deviceLatitude" class="longlat-input" />
           </el-form-item>
@@ -198,7 +204,8 @@
             <el-form-item label="设备端口:" prop="devicePort">
               <el-input v-model="form.devicePort" />
             </el-form-item>
-            <el-form-item v-if="form.deviceType !== 'platform' && isShowGbIdEditor" label="国标ID:" prop="gbId">
+            <!-- v-if="isShowGbIdEditor"  -->
+            <el-form-item label="国标ID:" prop="gbId">
               <el-input v-model="form.gbId" />
               <div class="form-tip">
                 用户可自行录入规范国标ID，未录入该项，平台会自动生成规范国标ID。
@@ -242,7 +249,7 @@
               />
             </el-form-item>
             <el-form-item
-              v-if="form.deviceType === 'nvr' || isIPC"
+              v-if="form.deviceType === 'nvr' || form.deviceType === 'platform' || isIPC"
               prop="transPriority"
             >
               <template slot="label">
@@ -554,6 +561,7 @@ export default class extends Mixins(createMixin) {
     createGa1400Certificate: false
   }
   private hasViewLib = false
+  private ifUseDeviceName = false
 
   public async mounted() {
     if (this.isUpdate || this.isChannel) {
@@ -565,6 +573,7 @@ export default class extends Mixins(createMixin) {
     this.getGbAccounts()
     this.getGa1400Accounts()
     this.onGroupChange()
+    this.setIfUseDeviceName()
   }
 
   /**
@@ -688,6 +697,14 @@ export default class extends Mixins(createMixin) {
     } finally {
       this.loading.account = false
     }
+  }
+
+  /**
+   * 获取是否使用设备名称
+   */
+  private setIfUseDeviceName() {
+    const temp = this.$store.state.user.userConfigInfo.find((item: any) => item.key === 'enableCloudChannelName')
+    this.ifUseDeviceName = Boolean(temp.value)
   }
 
   /**
