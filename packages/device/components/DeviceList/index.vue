b81ba3b65e0d267c2aae685f3aa29479be7180ce
xiaoren <xiaoren@chinatelecom.cn>
2022-08-11 15:37:20 +0800

    feat: palages/device dic文件

--- packages/device/components/DeviceList/index.vue
+++ packages/device/components/DeviceList/index.vue
@@ -1,15 +1,330 @@
 <template>
   <div class="device-container">
-    <div class="device-list__container" />
+    <div class="list-wrap">
+      <div class="device-info">
+        <info-list v-if="isNVR" label-width="80">
+          <info-list-item label="设备名称:">{{ deviceInfo.deviceName }}</info-list-item>
+          <info-list-item label="国标ID:">{{ deviceInfo.gbId }}</info-list-item>
+          <info-list-item label="设备状态:">
+            <status-badge :status="deviceInfo.deviceStatus" />
+            {{ deviceStatus[deviceInfo.deviceStatus] }}
+          </info-list-item>
+          <info-list-item label="创建时间:">{{ deviceInfo.createdTime }}</info-list-item>
+          <info-list-item v-if="deviceInfo.createSubDevice === 2" label="可支持通道数量:">{{ deviceInfo.deviceStats }}</info-list-item>
+          <info-list-item v-else label="通道数量:">{{ deviceInfo.deviceStats }}</info-list-item>
+          <info-list-item label="在线流数量:">{{ deviceInfo.deviceStats }}</info-list-item>
+        </info-list>
+        <info-list v-if="isPlatform" label-width="80">
+          <info-list-item label="平台名称:">{{ deviceInfo.deviceName }}</info-list-item>
+          <info-list-item label="国标ID:">{{ deviceInfo.gbId }}</info-list-item>
+          <info-list-item label="设备状态:">
+            <status-badge :status="deviceInfo.deviceStatus" />
+            {{ deviceStatus[deviceInfo.deviceStatus] }}
+          </info-list-item>
+          <info-list-item label="创建时间:">{{ deviceInfo.createdTime }}</info-list-item>
+        </info-list>
+      </div>
+      <div class="list-wrap__tools">
+        <div class="list-wrap__tools__left">
+          <el-button key="dir-button" type="primary" @click="1">{{ '添加设备' }}</el-button>
+          <el-button v-if="isNVR && checkPermission(['AdminDevice'])" key="dir-button" type="primary" @click="1">{{ '添加子设备' }}</el-button>
+          <el-button v-if="isNVR" key="check-nvr-detail" @click="1">查看NVR设备详情</el-button>
+          <el-button v-if="isNVR && checkPermission(['AdminDevice'])" key="edit-nvr" @click="1">编辑NVR设备</el-button>
+          <el-button v-if="isPlatform" key="check-platform" @click="1">查看Platform详情</el-button>
+          <el-button v-if="isPlatform && checkPermission(['AdminDevice'])" key="edit-platform" @click="1">编辑Platform</el-button>
+          <el-button v-if="isPlatform" key="sync" :loading="loading.syncDevice" @click="1">同步</el-button>
+          <el-dropdown v-if="checkPermission(['AdminDevice'], {id: dirId !== '0' ? dirId : currentGroupId})" placement="bottom" @command="1">
+            <el-button>导出<i class="el-icon-arrow-down el-icon--right" /></el-button>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item command="exportAll" :disabled="!deviceList.length">导出所有分页</el-dropdown-item>
+              <el-dropdown-item command="exportCurrentPage" :disabled="!deviceList.length">导出当前页</el-dropdown-item>
+              <el-dropdown-item command="exportSelect" :disabled="!selectedDeviceList.length">导出选定项</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <el-upload
+            ref="excelUpload"
+            action="#"
+            :show-file-list="false"
+            :http-request="uploadExcel"
+            class="import-button"
+          >
+            <el-button>导入</el-button>
+          </el-upload>
+          <el-button @click="1">下载模板</el-button>
+          <el-dropdown v-if="!isVGroup && checkPermission(['AdminDevice'], {id: dirId !== '0' ? dirId : currentGroupId})" key="dropdown" placement="bottom" @command="1">
+            <el-button :disabled="!selectedDeviceList.length">批量操作<i class="el-icon-arrow-down el-icon--right" /></el-button>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item command="move">移动至</el-dropdown-item>
+              <el-dropdown-item command="startDevice">启用流</el-dropdown-item>
+              <el-dropdown-item command="stopDevice">停用流</el-dropdown-item>
+              <el-dropdown-item command="delete">删除</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+        <div class="list-wrap__tools__right">
+          <el-input v-show="false" v-model="keyword" class="search-group" placeholder="请输入关键词">
+            <el-button slot="append" class="el-button-rect"><svg-icon name="search" /></el-button>
+          </el-input>
+          <el-button v-if="!isVGroup && !isChannel" class="el-button-rect sync-button" :disabled="false" :class="{'loading': false}" @click="1"><svg-icon name="refresh" />同步设备状态</el-button>
+        </div>
+      </div>
+      <div class="list-wrap__filter">
+        <div v-for="{key, value} in filterButtons" :key="key" class="list-wrap__filter-item" @click="1">
+          <label>{{ deviceParams[key] }}</label>
+          <span v-if="key === 'deviceType'">{{ deviceType[value] }}</span>
+          <span v-if="key === 'deviceStatus'">{{ deviceStatus[value] }}</span>
+          <span v-if="key === 'streamStatus'">{{ streamStatus[value] }}</span>
+          <span v-if="key === 'recordStatus'">{{ recordStatusFilterType[value] }}</span>
+          <svg-icon class="filter-button__close" name="close" width="10" height="10" />
+        </div>
+      </div>
+      <div class="list-wrap__body">
+        <el-table ref="deviceTable" :height="tableMaxHeight" :data="deviceList" fit class="list-wrap__body__table">
+          <el-table-column type="selection" prop="selection" class-name="col-selection" width="55" />
+          <el-table-column label="设备ID/名称" min-width="200">
+            <template slot-scope="{row}">
+              <div class="device-name">
+                <div class="device-id">{{ row.deviceId }}</div>
+                <div>{{ row.deviceName }}</div>
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column v-if="isNVR" label="通道号" min-width="100">
+            <template slot-scope="{row}">
+              {{ 'D' + row.channelNum }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            v-if="!isNVR"
+            key="deviceType"
+            column-key="deviceType"
+            prop="deviceType"
+            label="类型"
+            :filters="isIPC ? [] : filtersArray.deviceType"
+            :filter-multiple="false"
+          >
+            <template slot="header">
+              <span class="filter">类型</span>
+              <svg-icon v-if="!isIPC" class="filter" name="filter" width="15" height="15" />
+            </template>
+            <template slot-scope="{row}">
+              {{ deviceType[row.deviceType] }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            v-if="isGb"
+            key="deviceStatus"
+            column-key="deviceStatus"
+            label="设备状态"
+            min-width="110"
+            :filters="isIPC ? [] : filtersArray.deviceStatus"
+            :filter-multiple="false"
+          >
+            <template slot="header">
+              <span class="filter">设备状态</span>
+              <svg-icon v-if="!isIPC" class="filter" name="filter" width="15" height="15" />
+            </template>
+            <template slot-scope="{row}">
+              <status-badge :status="row.deviceStatus" />
+              {{ deviceStatus[row.deviceStatus] || '-' }}
+            </template>
+          </el-table-column>
+          <el-table-column v-if="isGb && ga1400Flag" key="inProtocol" prop="inProtocol" label="接入协议" min-width="100">
+            <template slot-scope="{row}">
+              {{ 'GB28181' + (row.apeId && '、GA1400') }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            key="streamStatus"
+            column-key="streamStatus"
+            prop="streamStatus"
+            label="流状态"
+            min-width="110"
+            :filters="isIPC ? [] : filtersArray.streamStatus"
+            :filter-multiple="false"
+          >
+            <template slot="header">
+              <span class="filter">流状态</span>
+              <svg-icon v-if="!isIPC" class="filter" name="filter" width="15" height="15" />
+            </template>
+            <template slot-scope="{row}">
+              <status-badge :status="row.streamStatus" />
+              {{ streamStatus[row.streamStatus] || '-' }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            key="recordStatus"
+            column-key="recordStatus"
+            prop="recordStatus"
+            label="录制状态"
+            min-width="110"
+            :filters="isIPC ? [] : filtersArray.recordStatus"
+            :filter-multiple="false"
+          >
+            <template slot="header">
+              <span class="filter">录制状态</span>
+              <svg-icon v-if="!isIPC" class="filter" name="filter" width="15" height="15" />
+            </template>
+            <template slot-scope="{row}">
+              <span v-if="row.deviceType === 'nvr'">-</span>
+              <span v-else><status-badge :status="recordStatusType[row.recordStatus]" />{{ recordStatus[row.recordStatus] || '-' }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column key="bitrate" min-width="100" prop="bitrate" label="当前码率">
+            <template slot-scope="{row}">
+              {{ row.bitrate ? (row.bitrate / 1024).toFixed(2) + 'Mbps' : '-' }}
+            </template>
+          </el-table-column>
+          <el-table-column key="errorMessage" prop="errorMessage" label="异常提示">
+            <template slot-scope="{row}">
+              {{ row.errorMessage || '-' }}
+            </template>
+          </el-table-column>
+          <el-table-column key="deviceVendor" prop="deviceVendor" label="厂商">
+            <template slot-scope="{row}">
+              {{ row.deviceVendor || '-' }}
+            </template>
+          </el-table-column>
+          <el-table-column v-if="isGb || isNVR" key="gbId" prop="gbId" label="国标ID" min-width="190">
+            <template slot-scope="{row}">
+              {{ row.gbId || '-' }}
+            </template>
+          </el-table-column>
+          <el-table-column v-if="ga1400Flag && (isGb || isNVR)" key="apeId" prop="apeId" label="视图编码" min-width="190">
+            <template slot-scope="{row}">
+              {{ row.apeId || '-' }}
+            </template>
+          </el-table-column>
+          <el-table-column v-if="isGb || !isNVR" key="sipTransType" prop="sipTransType" label="信令传输模式" min-width="110">
+            <template slot-scope="{row}">
+              {{ sipTransType[row.sipTransType] || '-' }}
+            </template>
+          </el-table-column>
+          <el-table-column v-if="isGb || !isNVR" key="streamTransType" prop="streamTransType" label="流传输模式" min-width="110">
+            <template slot-scope="{row}">
+              {{ streamTransType[row.streamTransType] || '-' }}
+            </template>
+          </el-table-column>
+          <el-table-column v-if="isGb || !isNVR" key="transPriority" prop="transPriority" label="优先TCP传输" min-width="110">
+            <template slot-scope="{row}">
+              {{ transPriority[row.transPriority] || '-' }}
+            </template>
+          </el-table-column>
+          <el-table-column v-if="isGb && !isNVR" key="tunnelNum" prop="tunnelNum" label="通道数">
+            <template slot-scope="{row}">
+              {{ row.deviceStats && row.deviceStats.channelSize || '-' }}
+            </template>
+          </el-table-column>
+          <el-table-column v-if="isGb && !isNVR" key="deviceIp" label="设备IP" min-width="130">
+            <template slot-scope="{row}">
+              {{ row.deviceIp || '-' }}
+            </template>
+          </el-table-column>
+          <el-table-column v-if="isGb && !isNVR" key="devicePort" label="设备端口">
+            <template slot-scope="{row}">
+              {{ row.devicePort || '-' }}
+            </template>
+          </el-table-column>
+          <el-table-column key="createdTime" label="创建时间" min-width="180">
+            <template slot-scope="{row}">
+              {{ row.createdTime }}
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" prop="action" class-name="col-action" width="270" fixed="right">
+            <template slot-scope="scope">
+              <el-button v-if="checkPermission(['ScreenPreview'])" type="text" :disabled="scope.row.deviceType === 'nvr' || scope.row.deviceType === 'platform'" @click="1">实时预览</el-button>
+              <el-button v-if="checkPermission(['ReplayRecord'])" type="text" :disabled="scope.row.deviceType === 'nvr' || scope.row.deviceType === 'platform'" @click="1">录像回放</el-button>
+              <!-- <el-button type="text" disabled @click="goToPreview('snapshot', scope.row)">查看截图</el-button> -->
+              <el-popover
+                width="400"
+                :open-delay="800"
+                trigger="hover"
+                @show="getEventsList(scope.row)"
+              >
+                <el-table
+                  v-loading="loading.events"
+                  :show-header="false"
+                  :data="eventsList"
+                  :height="170"
+                  empty-text="暂无设备事件"
+                >
+                  <el-table-column property="createdTime" />
+                  <el-table-column property="errorMessage" />
+                </el-table>
+                <el-button slot="reference" type="text" @click="deviceRouter({id: scope.row.deviceId, type: 'detail', activeName: 'events'})">设备事件</el-button>
+              </el-popover>
+              <el-dropdown @command="handleMore">
+                <el-button type="text">更多<i class="el-icon-arrow-down" /></el-button>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item v-if="scope.row.deviceType === 'nvr'" :command="{type: 'nvr', device: scope.row}">查看通道</el-dropdown-item>
+                  <el-dropdown-item v-if="isNVR" :command="{type: 'detail', device: scope.row}">通道详情</el-dropdown-item>
+                  <el-dropdown-item v-else :command="{type: 'detail', device: scope.row}">设备详情</el-dropdown-item>
+                  <template v-if="scope.row.deviceType === 'ipc'">
+                    <el-dropdown-item v-if="!isVGroup && scope.row.streamStatus === 'on' && checkPermission(['AdminDevice'])" :command="{type: 'stopDevice', device: scope.row}">停用流</el-dropdown-item>
+                    <el-dropdown-item v-else-if="!isVGroup && checkPermission(['AdminDevice'])" :command="{type: 'startDevice', device: scope.row}">启用流</el-dropdown-item>
+                    <el-dropdown-item v-if="!isVGroup && recordStatusType[scope.row.recordStatus] === 'on' && checkPermission(['AdminDevice'])" :command="{type: 'stopRecord', device: scope.row}">停止录像</el-dropdown-item>
+                    <el-dropdown-item v-else-if="!isVGroup && checkPermission(['AdminDevice'])" :command="{type: 'startRecord', device: scope.row}">开始录像</el-dropdown-item>
+                  </template>
+                  <el-dropdown-item v-if="!isVGroup && checkPermission(['AdminDevice'])" :command="{type: 'updateResource', device: scope.row}">配置资源包</el-dropdown-item>
+                  <el-dropdown-item v-if="!isVGroup && (!isNVR && scope.row.parentDeviceId === '-1') && checkPermission(['AdminDevice'])" :command="{type: 'move', device: scope.row}">移动至</el-dropdown-item>
+                  <el-dropdown-item v-if="!isVGroup && checkPermission(['AdminDevice'])" :command="{type: 'update', device: scope.row}">编辑</el-dropdown-item>
+                  <!--自动创建的子通道不允许删除-->
+                  <el-dropdown-item v-if="!isVGroup && isAllowedDelete && checkPermission(['AdminDevice'])" :command="{type: 'delete', device: scope.row}">删除</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-pagination />
+        <div v-if="isNVR" class="el-pagination">
+          <div class="el-pagination__total">共{{ deviceList.length }}条</div>
+        </div>
+        <div v-if="!deviceList.length && !loading.list" class="list-wrap__body__empty-text">
+          当前{{ isNVR ? 'NVR' : '目录' }}暂无设备
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import { Device } from '../../type/Device'
+import { DeviceParams, DeviceStatus, StreamStatus, RecordStatus, RecordStatusType, RecordStatusFilterType, DeviceGb28181Type, SipTransType, StreamTransType, TransPriority } from '../../dics/index'
+import { checkPermission } from '@/utils/permission'
 
 @Component({
   name: 'DeviceList'
 })
 export default class extends Vue {
+  private checkPermission = checkPermission
+  private deviceParams = DeviceParams
+  private deviceStatus = DeviceStatus
+  private streamStatus = StreamStatus
+  private recordStatus = RecordStatus
+  private recordStatusType = RecordStatusType
+  private recordStatusFilterType = RecordStatusFilterType
+  private deviceType = DeviceGb28181Type
+  private sipTransType = SipTransType
+  private streamTransType = StreamTransType
+  private transPriority = TransPriority
+
+  private get isNVR() {
+    return true
+  }
+
+  private get isPlatform() {
+    return false
+  }
+
+  private get isVGroup() {
+    return false
+  }
+
+  private deviceInfo: any = {}
+
+  private deviceList: Array<Device> = []
+
+  private selectedDeviceList: Array<Device> = []
 }
 </script>
---
+++ packages/device/dics/index.ts
@@ -0,0 +1,311 @@
+export const PlatformStatus = {
+  'on': '在线',
+  'off': '离线'
+}
+
+export const DeviceStatus = {
+  'on': '在线',
+  'off': '离线',
+  'new': '未注册'
+}
+
+export const DeviceGb28181Type = {
+  'ipc': 'IPC',
+  'nvr': 'NVR',
+  'platform': 'Platform'
+}
+
+export const DeviceRtmpType = {
+  'ipc': 'IPC',
+  'platform': 'Platform'
+}
+
+export const DeviceRtspType = {
+  'ipc': 'IPC',
+  'nvr': 'NVR'
+}
+
+export const InType = {
+  'push': '推流',
+  'pull': '拉流'
+}
+
+export const InProtocolType = {
+  'gb28181': 'GB28181',
+  'rtmp': 'RTMP',
+  'onvif': 'ONVIF',
+  'rtsp': 'RTSP',
+  'ehome': 'EHOME'
+}
+
+export const OutProtocolType = {
+  'rtmp': 'RTMP',
+  'hls': 'HLS',
+  'flv': 'FLV',
+  'webrtc': 'WebRTC'
+}
+
+export const GroupStatus = {
+  'on': '已启用',
+  'off': '已停用'
+}
+
+export const AuthStatus = {
+  true: '开启',
+  false: '关闭'
+}
+
+export const AnonymousType = {
+  'anonymous': '匿名',
+  'normal': '非匿名'
+}
+
+export const RecordStorageType = {
+  'MP4': 'mp4',
+  'FLV': 'flv',
+  'HLS': 'hls'
+}
+
+export const SnapshotStorageType: { [key: string]: string } = {
+  'cover': '覆盖式截图',
+  'realtime': '实时截图'
+}
+
+export const SecretStatus = {
+  true: '已启用',
+  false: '已禁用'
+}
+
+export const PullType = {
+  1: '已启用',
+  2: '已禁用'
+}
+
+export const PushType = {
+  1: '已启用',
+  2: '已禁用'
+}
+
+export const CreateSubDevice = {
+  1: '是',
+  2: '否'
+}
+
+export const SipTransType = {
+  tcp: 'TCP',
+  udp: 'UDP'
+}
+
+export const StreamTransType = {
+  ps_rtp_udp: 'UDP',
+  ps_rtp_tcp: 'TCP',
+  h264_rtp_udp: 'UDP',
+  h264_rtp_tcp: 'TCP',
+  h265_rtp_udp: 'UDP',
+  h265_rtp_tcp: 'TCP'
+}
+
+export const TransPriority = {
+  tcp: '是',
+  udp: '否'
+}
+
+export const StreamStatus = {
+  'on': '在线',
+  'off': '离线',
+  'failed': '失败'
+}
+
+export const RecordStatus = {
+  0: '未录制',
+  1: '录制中',
+  2: '录制中',
+  3: '录制失败'
+}
+
+export const RecordStatusType: { [key: number]: any } = {
+  0: 'off',
+  1: 'on',
+  2: 'on',
+  3: 'error'
+}
+
+export const RecordStatusFilterType = {
+  'off': '未录制',
+  'on': '录制中',
+  'error': '录制失败'
+}
+
+export const StreamType = {
+  1: '全量视频',
+  2: '移动侦测'
+}
+
+export const DeviceParams = {
+  'deviceType': '类型',
+  'deviceStatus': '设备状态',
+  'streamStatus': '流状态',
+  'recordStatus': '录制状态'
+}
+
+export const AlertLevel = {
+  normal: '一般警告',
+  serious: '严重警告'
+}
+
+export const AlertIcon = {
+  normal: 'warning',
+  serious: 'alert'
+}
+
+/**
+ * 视图库
+ */
+export const ViewLibStatus: any = {
+  'on': '在线',
+  'off': '离线'
+}
+
+/**
+ * AI告警类型
+ * 相应修改src/utils/device.ts
+ */
+export const AlertType: any = {
+  // 1: '未戴口罩',
+  6: '口罩检测', // 10003
+  8: '人员聚集', // 10005
+  // 2: '人员聚集',
+  // 3: '人员布控',
+  4: '人脸识别', // 10001
+  5: '吸烟检测', // 10002
+  7: '安全帽反光服检测', // 10004
+  9: '危险区域检测', // 10006
+  10: '烟雾明火检测', // 10007
+  11: '冲压机检测', // 10008
+  12: '人体属性检测', // 10009
+  13: '蜜蜂密度检测', // 10010
+  14: '铁塔基站设备检测',
+  15: '铁塔基站天线检测', // 10012
+  16: '医疗防护服检测',
+  17: '车牌识别',
+  18: '棉花检测',
+  19: '入侵检测',
+  20: '在场人员+口罩检测',
+  21: '垃圾检测',
+  23: '电动车检测',
+  22: '车辆统计',
+  24: '车辆违停检测',
+  25: '车辆拥堵检测',
+  26: '人群感应检测',
+  27: '实时在岗检测',
+  32: '人车流量统计',
+  28: '人员徘徊检测',
+  29: '垃圾站检测',
+  30: '视野遮挡检测',
+  31: '跌倒检测',
+  33: '动物检测'
+}
+
+export const AiMaskType: any = {
+  0: '未戴口罩',
+  1: '戴口罩',
+  2: '佩戴不规范'
+}
+
+export const AiAttribute: any = {
+  'gender': '性别',
+  'facemask': '是否戴口罩',
+  'attachment': '附属物',
+  'age': '年龄',
+  'direction': '方向',
+  'upperwear': '上身服饰',
+  'lowerwear': '下身服饰',
+  'uppercolor': '上身服饰颜色',
+  'lowercolor': '下身服饰颜色'
+}
+
+export const AlertParams = {
+  'alertLevel': '告警级别',
+  'alertType': '告警类型'
+}
+
+export const ResourceType = {
+  'VSS_VIDEO': '视频包',
+  'VSS_UPLOAD_BW': '上行带宽包',
+  'VSS_DOWNLOAD_BW': '下行带宽包',
+  'VSS_AI': 'AI包'
+}
+
+export const ResourceAiType = {
+  'AI-100': '分钟级',
+  'AI-200': '秒级',
+  'AI-300': '高算力型'
+}
+
+// AI分析频率
+export const ResultTimeInterval = {
+  '1秒': 1,
+  '5秒': 5,
+  '10秒': 10,
+  '20秒': 20,
+  '30秒': 30,
+  '1分钟': 60,
+  '5分钟': 300,
+  '10分钟': 600
+}
+
+/**
+ *  设备事件级别
+ */
+export const errorLevel = {
+  '0': '全部',
+  '1': '异常',
+  '2': '正常'
+}
+
+/**
+ *  设备事件类型
+ */
+export const eventsType = {
+  'all': '全部',
+  'device': '设备事件',
+  'stream': '流事件',
+  'record': '录制事件',
+  'coderate_overflow': '超码率'
+}
+
+/**
+ * 视频播放比例
+ */
+export const scaleKind = [
+  { label: '16:9', kind: '16 / 9', num: '3' },
+  { label: '4:3', kind: '4 / 3', num: '4' },
+  { label: '原始比例', kind: 'normal', num: '2' },
+  { label: '拉伸', kind: 'fit', num: '1' }
+]
+/**
+ * 垃圾类别
+ */
+export const TrashType = [
+  { label: 'cask_yes', cname: '垃圾桶已盖' },
+  { label: 'cask_no', cname: '垃圾桶未盖' },
+  { label: 'cask_overflows', cname: '垃圾桶溢满' },
+  { label: 'trash', cname: '地面垃圾' }
+]
+
+export const ViewTypes = [
+  { value: '0', cname: '全部' },
+  { value: '1', cname: '人员' },
+  { value: '2', cname: '人脸' },
+  { value: '3', cname: '机动车' },
+  { value: '4', cname: '非机动车' }
+]
+
+export const HelmetClothType = [
+  { label: 'helmet', cname: '未带安全帽告警' },
+  { label: 'reflective', cname: '未穿反光服告警' }
+]
+
+export const AnimalType = [
+  { label: 'Bear', cname: '狗熊' }
+]
