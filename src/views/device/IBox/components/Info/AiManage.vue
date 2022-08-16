7825fa3e515e1570de185bf09ccb6d9572a4be6d
liugongjian <liugongjianxin@163.com>
2022-08-16 15:05:37 +0800

    '+AiManage'

---
+++ src/views/device/IBox/components/Info/AiManage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="ibox-list" />
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'DeviceList',
+  components: {
+  }
+})
+
+export default class IBoxList extends Vue {
+  public tableData = []
+
+  async mounted() {
+
+  }
+}
+</script>
