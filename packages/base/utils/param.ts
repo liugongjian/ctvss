2a440aa1d898d67e9c6276dd194b4618c71b6cfa
Linghao Zhu <>
2022-09-30 11:00:58 +0800

    feat: 更新base版本

--- packages/base/utils/param.ts
+++ packages/base/utils/param.ts
@@ -5,7 +5,7 @@ enum Method {
 
 const recursionData = (data, method) => {
   const newObj = Array.isArray(data) ? [] : {}
-  for (let key in data) {
+  for (const key in data) {
     let firstChar = key.substring(0, 1)
     firstChar = method === Method.ToLowerCase ? firstChar.toLowerCase() : firstChar.toUpperCase()
     const newKey = firstChar + key.substring(1, key.length)
