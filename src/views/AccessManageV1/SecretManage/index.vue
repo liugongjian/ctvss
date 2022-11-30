870bebd31a6e71579463809d7130f5da886d184f
Linghao Zhu <>
2022-11-30 09:44:36 +0800

    feat: 恢复AccessManager V1版本

--- src/views/AccessManageV1/SecretManage/index.vue
+++ src/views/AccessManageV1/SecretManage/index.vue
@@ -21,7 +21,7 @@
           </div>
           <el-table :data="dataList" fit>
             <el-table-column label="密钥" min-width="300">
-              <template slot-scope="{ row }">
+              <template slot-scope="{row}">
                 <div>
                   <span>{{ 'AccessKeyId: ' + row.accessKey }}</span>
                   <el-button v-clipboard:copy="row.accessKey" v-clipboard:success="copySuccess" v-clipboard:error="copyError" type="text" class="ml10">
@@ -45,7 +45,7 @@
             <el-table-column prop="createdTime" label="创建时间" min-width="160" />
             <el-table-column prop="updatedTime" label="上次访问时间" min-width="160" />
             <el-table-column label="状态">
-              <template slot-scope="{ row }">
+              <template slot-scope="{row}">
                 <status-badge :status="row.status ? 'on' : 'warning'" />
                 {{ secretStatus[row.status] || "已启用" }}
               </template>
@@ -144,8 +144,7 @@ export default class extends Vue {
       headline: '',
       id: '',
       accessKey: '',
-      secretKey: '',
-      description: ''
+      secretKey: ''
     }
     this.showTipDialog = false
   }
@@ -249,8 +248,7 @@ export default class extends Vue {
         headline: '新建密钥成功',
         id: res.id,
         accessKey: res.accessKey,
-        secretKey: res.secretKey,
-        description: ''
+        secretKey: res.secretKey
       }
       this.showTipDialog = true
     } catch (e) {
@@ -312,8 +310,7 @@ export default class extends Vue {
             headline: 'OpenAPI授权成功',
             id: res.id,
             accessKey: res.accessKey,
-            secretKey: res.secretKey,
-            description: ''
+            secretKey: res.secretKey
           }
           this.showTipDialog = true
         }
