4eddb723c2d10b3c08ec37bc0c1abdc63590b040
zhulh <zhulh@chinatelecom.cn>
2022-12-13 10:53:16 +0800

    feat: mp4播放

--- src/views/device/components/ReplayPlayer/index.vue
+++ src/views/device/components/ReplayPlayer/index.vue
@@ -123,7 +123,7 @@ export default class extends Vue {
   private onChange() {
     if (this.screen.recordType === 0) {
       this.url = this.recordManager.currentRecord && this.recordManager.currentRecord.url
-      this.type = this.url.endsWith('mp4') ? 'mp4' : 'hls'
+      this.type = this.recordManager.currentRecord.fileFormat
       this.codec = this.recordManager.currentRecord && this.recordManager.currentRecord.codec
     } else {
       this.url = this.screen.url
--- src/views/device/services/Record/RecordManager.ts
+++ src/views/device/services/Record/RecordManager.ts
@@ -377,10 +377,11 @@ export class RecordManager {
         startTime: getTimestamp(record.startTime) / 1000,
         endTime: getTimestamp(record.endTime) / 1000,
         duration: record.duration,
-        url: record.fileFormat === 'mp4' ? record.playUrl.mp4Url : record.playUrl.hlsUrl,
+        url: record.playUrl[`${record.fileFormat}Url`],
         codec: record.video.codec,
         templateName: record.templateName,
-        cover: record.cover
+        cover: record.cover,
+        fileFormat: record.fileFormat
       })
     })
   }
