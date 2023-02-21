24372ae48d752e99df6703b42189263167423af7
tangxinwei <182191766@qq.com>
2023-02-21 15:11:26 +0800

    Merge branch 'feat-Sprint12-liuzhou_record_lock' into release-Sprint12-liuzhou
    # Conflicts:
    #	package-lock.json
    #	src/router/index.ts
    #	src/settings.ts
    #	src/views/components/TemplateBind.vue
    #	src/views/device/components/ScreenBoard/ScreenTree.vue
    #	src/views/device/mixin/indexMixin.ts
    #	src/views/device/services/Record/Record.ts
    #	src/views/device/services/Record/RecordManager.ts
    #	src/views/group/config.vue

--- src/views/device/components/ScreenBoard/ScreenList.vue
+++ src/views/device/components/ScreenBoard/ScreenList.vue
@@ -68,7 +68,7 @@
           <el-table-column prop="action" label="操作" width="200" fixed="right">
             <template slot-scope="{row}">
               <el-button
-                v-if="!isVGroup && checkPermission(['AdminRecord'])"
+                v-if="!isVGroup && checkPermission(['ivs:GetCloudRecord'])"
                 :disabled="row.loading || (!canLock && row.isLock === 1)"
                 type="text"
                 @click="downloadReplay(row)"
