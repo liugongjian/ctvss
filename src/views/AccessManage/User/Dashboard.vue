deef65373110dcc6a01c68c88015e0167c800076
zhaodan <zhaodan@telecom.cn>
2023-02-21 16:17:53 +0800

    fix: VSS-8513 点击弹窗蒙层逻辑修改

--- src/views/AccessManage/User/Dashboard.vue
+++ src/views/AccessManage/User/Dashboard.vue
@@ -2,7 +2,7 @@
  * @Author: zhaodan zhaodan@telecom.cn
  * @Date: 2023-02-14 16:58:39
  * @LastEditors: zhaodan zhaodan@telecom.cn
- * @LastEditTime: 2023-02-21 14:05:58
+ * @LastEditTime: 2023-02-21 16:17:11
  * @FilePath: /vss-user-web/src/views/AccessManage/User/Dashboard.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -90,6 +90,7 @@
       :before-close="closePasswordDialog"
       :destroy-on-close="true"
       custom-class="dashboard__set-password"
+      :close-on-click-modal="false"
       width="40%"
     >
       <el-form
