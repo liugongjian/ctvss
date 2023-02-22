0506ea36178218861c37e337c0378df82fed1079
liugongjian <liugongjianxin@163.com>
2023-02-22 14:12:39 +0800

    'fix:云台缩放操作添加参数'

--- src/components/VssPlayer/components/PtzZoom.vue
+++ src/components/VssPlayer/components/PtzZoom.vue
@@ -13,6 +13,7 @@ import { StreamInfo, DeviceInfo } from '@/components/VssPlayer/types/VssPlayer'
 import ComponentMixin from './mixin'
 import { throttle } from 'lodash'
 import ResizeObserver from 'resize-observer-polyfill'
+import { GroupModule } from '@/store/modules/group'
 
 @Component({
   name: 'PtzZoom'
@@ -231,7 +232,9 @@ export default class extends ComponentMixin {
       midPointX,
       midPointY,
       lengthX,
-      lengthY
+      lengthY,
+      inProtocol: this.deviceInfo.inProtocol,
+      groupId: GroupModule.group?.groupId
     }
     if (lengthX !== '0' || lengthY !== '0') {
       dragCanvasZoom(param).then(() => {
