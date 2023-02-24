f1ae95d827f9946e9f12b53a31085fb0a0d661cd
zhaodan <zhaodan@telecom.cn>
2023-02-24 10:48:12 +0800

    feat:修改统计信息 录像丢失片段 列表展示

--- src/views/Statistic/components/MissTable.vue
+++ src/views/Statistic/components/MissTable.vue
@@ -2,7 +2,7 @@
  * @Author: zhaodan zhaodan@telecom.cn
  * @Date: 2023-02-15 15:36:02
  * @LastEditors: zhaodan zhaodan@telecom.cn
- * @LastEditTime: 2023-02-16 15:46:51
+ * @LastEditTime: 2023-02-24 10:47:42
  * @FilePath: /vss-user-web/src/views/Statistic/components/MissTable.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -25,20 +25,21 @@
       />
       <el-table-column
         prop="missSeconds"
-        label="缺失时长(秒)"
+        label="缺失时长"
       >
         <template slot-scope="{row}">
-          <span style="margin-left: 10px;">{{ row.missSeconds }}</span>
+          <span>{{ row.missSeconds }}秒</span>
+          <span>({{ durationFormat(row.missSeconds) }})</span>
         </template>
       </el-table-column>
-      <el-table-column
+      <!-- <el-table-column
         prop="missSeconds"
         label="缺失时长"
       >
         <template slot-scope="{row}">
           <span style="margin-left: 10px;">{{ durationFormat(row.missSeconds) }}</span>
         </template>
-      </el-table-column>
+      </el-table-column> -->
     </el-table>
     <el-pagination
       :current-page="pager.pageNum"
