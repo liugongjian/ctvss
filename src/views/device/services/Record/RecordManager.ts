810925a30cca9b05b4d9a5f66ae85e4650066d86
tangxinwei <182191766@qq.com>
2023-03-03 10:01:37 +0800

    fix: 代码编译提出录像权限相关修复代码

--- src/views/device/services/Record/RecordManager.ts
+++ src/views/device/services/Record/RecordManager.ts
@@ -327,7 +327,7 @@ export class RecordManager {
       }
     } catch (e) {
       console.log('e ', e.code)
-      if (e.code === this.screen.ERROR.CANNOT_GET_RECORD || e.code === this.screen.ERROR_CODE.NO_RECORD || e.code === this.screen.ERROR_CODE.OUT_OF_RANGE || e.code === this.screen.ERROR_CODE.LOCKED) {
+      if (e.code === this.screen.ERROR_CODE.NO_RECORD || e.code === this.screen.ERROR_CODE.OUT_OF_RANGE || e.code === this.screen.ERROR_CODE.LOCKED) {
         this.screen.errorMsg = e.message
       }
       this.screen.currentRecordDatetime = time
