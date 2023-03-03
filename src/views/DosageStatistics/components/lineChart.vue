e7ddc31b2874b49e5696ad63164912ebde36047a
zhaodan <zhaodan@telecom.cn>
2023-03-03 18:21:05 +0800

    折线图基本完成

--- src/views/DosageStatistics/components/lineChart.vue
+++ src/views/DosageStatistics/components/lineChart.vue
@@ -2,12 +2,12 @@
  * @Author: zhaodan zhaodan@telecom.cn
  * @Date: 2023-03-02 14:17:04
  * @LastEditors: zhaodan zhaodan@telecom.cn
- * @LastEditTime: 2023-03-02 19:37:16
+ * @LastEditTime: 2023-03-03 16:27:14
  * @FilePath: /vss-user-web/src/views/DosageStatistics/components/lineChart.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-  <div id="containerLine"></div>
+  <div id="containerLine" class="line-chart"></div>
 </template>
 
 <script lang="ts">
@@ -17,7 +17,6 @@ import {
   Component
 } from 'vue-property-decorator'
 import { Chart } from '@antv/g2'
-import { TooltipItem } from '@antv/g2/lib/interface'
 
 @Component({
   name: 'LineChart',
@@ -27,7 +26,7 @@ export default class extends Vue {
   private chartData = [
     {
       date: '2023-01-23',
-      total: 11,
+      total: 110,
       use: 10
     },
     {
@@ -79,25 +78,48 @@ export default class extends Vue {
 
     this.chart.scale({
       use: {
-        range: [0, 1],
-        nice: true
+        range: [0.05, 0.95],
+        min: 0,
+        max: 100,
+        sync: true,
+        nice: true,
+        type: 'log'
       },
       total: {
-        range: [0, 1],
-        nice: true
+        range: [0.05, 0.95],
+        min: 0,
+        max: 100,
+        sync: true,
+        nice: true,
+        type: 'log'
+      },
+      date: {
+        // 图表距离周边距离
+        range: [0.05, 0.95]
       }
     })
 
     this.chart.axis('use', false)
+
+    this.chart.axis('date', {
+      label: {
+        rotate: this.chartData.length / 3 > 10 ? -45 : 0,
+        //x轴日期根据数据量判断是否逆时针旋转一定角度，需设置autoRotate:false逆时针才会生效
+        autoRotate: false,
+        offset: 30, //文本偏移量，避免旋转后，文本与x轴重叠
+        autoHide: false, //是否隐藏部分
+        autoEllipsis: false //是否省略
+      }
+    })
 
     this.chart.legend({
       custom: true,
       items: [
         {
           name: 'total',
-          value: 'total',
-          maker: {
-            symbol: 'line',
+          // value: 'total',
+          marker: {
+            symbol: 'hyphen',
             style: {
               stroke: this.lineColor.total,
               lineWidth: 2
@@ -106,9 +128,9 @@ export default class extends Vue {
         },
         {
           name: 'use',
-          value: 'use',
+          // value: 'use',
           marker: {
-            symbol: 'line',
+            symbol: 'hyphen',
             style: {
               stroke: this.lineColor.use,
               lineWidth: 2
@@ -118,50 +140,31 @@ export default class extends Vue {
       ]
     })
 
-    this.chart.line().position('date*total').color(this.lineColor.total)
-    this.chart.line().position('date*use').color(this.lineColor.use)
+    this.chart
+      .line()
+      .position('date*total')
+      .color(this.lineColor.total)
+      .tooltip('date*total*use')
+
+    this.chart
+      .line()
+      .position('date*use')
+      .color(this.lineColor.use)
+      .tooltip('date*use')
 
     this.chart.tooltip({
       // showCrosshairs: true, // 展示 Tooltip 辅助线
-      // 自定义  https://g2-v4.antv.vision/zh/docs/api/general/tooltip#tooltipcfgcustomitems--items-tooltipitem--tooltipitem
-      customItems: (originiItems: TooltipItem[]) => {
-        const items: any = originiItems
-
-        if (items[0].name === 'total') {
-          items[1] = {
-            ...items[0],
-            ...{
-              name: 'use',
-              value: items[0].data.use,
-              color: this.lineColor.use,
-              mappingData: {
-                color: this.lineColor.total,
-                nextPoints: items[0].mappingData.nextPoints,
-                points: items[0].mappingData.points,
-                x: items[0].mappingData.x,
-                y: items[0].mappingData.y
-              }
-            }
-          }
-        } else {
-          items[0].mappingData.color = this.lineColor.use
-        }
-        console.log('items--0--->', items)
-        return items
-      },
-      // itemTpl: `<div class="g2-tooltip">
-      //   <div class="g2-tooltip-title">{date}</div>
-      //   <ul class="g2-tooltip-list">
-      //     <li class="g2-tooltip-list-item">
-      //       <span class="g2-tooltip-marker"></span>
-      //       <span class="g2-tooltip-name">total</span>:<span class="g2-tooltip-value">{total}</span>
-      //     </li>
-      //     <li class="g2-tooltip-list-item">
-      //       <span class="g2-tooltip-marker"></span>
-      //       <span class="g2-tooltip-name">use</span>:<span class="g2-tooltip-value">{use}</span>
-      //     </li>
-      //   </ul>
-      // </div>`
+      // shared: true,
+      containerTpl: `
+        <div class="g2-tooltip">
+          <div class="g2-tooltip-title"></div>
+          <ul class="g2-tooltip-list"></ul>
+        </div>`,
+      itemTpl: `
+          <li class="g2-tooltip-list-item item-{name}">
+            <span class="g2-tooltip-marker" ></span>
+            <span class="g2-tooltip-name">{name}</span>:<span class="g2-tooltip-value">{value}</span>
+          </li>`
     })
 
     this.chart
@@ -172,25 +175,39 @@ export default class extends Vue {
       })
       .shape('circle')
       .style({ stroke: '#fff', lineWidth: 1 })
+      .tooltip('date*total*use')
+
     this.chart
       .point()
       .position('date*use')
       .color(this.lineColor.use)
       .shape('circle')
       .style({ stroke: '#fff', lineWidth: 1 })
-
-    // this.chart.point().position('date*total*use').tooltip({
-    //   fields: ['date', 'total', 'use'],
-    //   callback: (date, total, use)=>{
-    //     return {
-    //       date, total, use
-    //     }
-    //   }
-    // })
+      .tooltip('date*use')
 
     // this.chart.removeInteraction('tooltip')
-    this.chart.removeInteraction('legend-filter')
-    this.chart.interaction('legend-visible-filter')
+    // this.chart.removeInteraction('legend-filter')
+    // this.chart.interaction('legend-visible-filter')
+
+    this.chart.interaction('element-highlight')
+
+    this.chart.interaction('active-region')
+
+    // legend 图例的点击事件 因为line自定义而不起作用，所以，曲线救国
+    this.chart.on('legend-item:click', (ev: any) => {
+      const item = ev?.target?.get('delegateObject').item
+      const id = item?.id
+
+      // 使用find 默认返回第一个，也就是line，实际上有两个值 一个是line 一个是point
+      const graph = this.chart.geometries.filter(
+        (w) => w.getAttribute('position').getFields()[1] === id
+      )
+      if (graph) {
+        graph.forEach((ele) => {
+          ele.changeVisible(!item.unchecked)
+        })
+      }
+    })
 
     this.chart.render()
 
@@ -198,3 +215,24 @@ export default class extends Vue {
   }
 }
 </script>
+<style lang="scss" scoped>
+.line-chart {
+  ::v-deep .g2-tooltip {
+    .item-date {
+      display: none;
+    }
+
+    .item-total {
+      .g2-tooltip-marker {
+        background-color: #1890ff;
+      }
+    }
+
+    .item-use {
+      .g2-tooltip-marker {
+        background-color: #2fc25b;
+      }
+    }
+  }
+}
+</style>
