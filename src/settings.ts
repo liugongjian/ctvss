558cdedd9c3e1d05969c25ff38c3451535847e66
龙虾狂魔 <672412250@qq.com>
2023-03-10 09:36:54 +0800

    opt: 测试环境ip添加到互联网访问

--- src/settings.ts
+++ src/settings.ts
@@ -32,6 +32,7 @@ const settings: ISettings = {
   projectPrefix: '/vss',
   outNetworkWhiteList: [
     '182.43.127.35',
+    '182.43.127.36',
     'console.vcn.ctyun.cn',
     'console.vcn.ctcdn.cn'
   ],
