8c3dc7fbc236be7f5996a89d05732441305c42e6
zhaodan <zhaodan@telecom.cn>
2023-03-14 10:04:44 +0800

    图表优化

--- src/views/DosageStatistics/components/LineChart.vue
+++ src/views/DosageStatistics/components/LineChart.vue
@@ -2,7 +2,7 @@
  * @Author: zhaodan zhaodan@telecom.cn
  * @Date: 2023-03-02 14:17:04
  * @LastEditors: zhaodan zhaodan@telecom.cn
- * @LastEditTime: 2023-03-09 20:41:38
+ * @LastEditTime: 2023-03-13 17:56:54
  * @FilePath: /vss-user-web/src/views/DosageStatistics/components/lineChart.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
