e4410a3c3cf2ec53f5a8155458f4d7a481542d13
zhaodan <zhaodan@telecom.cn>
2023-04-13 16:57:32 +0800

    概览页文案修改

--- src/dics/periodLine.ts
+++ src/dics/periodLine.ts
@@ -2,20 +2,20 @@
  * @Author: zhaodan zhaodan@telecom.cn
  * @Date: 2023-03-24 10:09:38
  * @LastEditors: zhaodan zhaodan@telecom.cn
- * @LastEditTime: 2023-04-13 09:59:53
+ * @LastEditTime: 2023-04-13 16:56:45
  * @FilePath: /vss-user-web/src/dics/periodLine.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export const Options = {
   bandwidth: [
     {
-      value: 'upload_bandwidth',
-      label: '上行带宽',
+      value: 'upload_traffic',
+      label: '上行流量',
       kind: 'bandwidth'
     },
     {
-      value: 'upload_traffic',
-      label: '上行流量',
+      value: 'upload_bandwidth',
+      label: '上行带宽',
       kind: 'bandwidth'
     },
     {
