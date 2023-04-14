b2bf9f355dc6918183363aa8a6b4eba1bf7cbbe4
zhaodan <zhaodan@telecom.cn>
2023-04-14 15:50:29 +0800

    fix:访问限制 访问日志抽屉 样式优化

--- src/views/AccessManage/AccessRestriction/components/ListDrawer.vue
+++ src/views/AccessManage/AccessRestriction/components/ListDrawer.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: zhaodan zhaodan@telecom.cn
+ * @Date: 2023-04-14 14:24:16
+ * @LastEditors: zhaodan zhaodan@telecom.cn
+ * @LastEditTime: 2023-04-14 15:44:55
+ * @FilePath: /vss-user-web/src/views/AccessManage/AccessRestriction/components/ListDrawer.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <el-drawer
     :title="`${drawerInfo.userName}访问日志`"
@@ -34,7 +42,7 @@ export default class extends Vue {
 
   ::v-deep .el-pagination {
     position: fixed;
-    bottom: 26px;
+    bottom: 36px;
     right: 26px;
   }
 }
