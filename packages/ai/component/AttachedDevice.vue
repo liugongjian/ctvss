d3fef6b0d134488a304371d9f2944e6539af8c73
liugongjian <liugongjianxin@163.com>
2023-04-21 15:43:26 +0800

    'feat:部分ai模块移入packages'

--- packages/ai/component/AttachedDevice.vue
+++ packages/ai/component/AttachedDevice.vue
@@ -3,7 +3,11 @@
     <div class="alarm-container">
       <div class="alarm-container__time">
         告警时间：
-        <el-radio-group v-model="period.periodType" size="medium" @change="handleChange">
+        <el-radio-group
+          v-model="period.periodType"
+          size="medium"
+          @change="handleChange"
+        >
           <!-- <el-radio-group> -->
           <el-radio-button label="今天" />
           <el-radio-button label="近3天" />
@@ -36,8 +40,16 @@
       <el-table-column label="名称">
         <template slot-scope="scope">
           <div class="device-list__device-type">
-            <status-badge v-if="scope.row.deviceType === 'ipc'" :status="scope.row.deviceStatus" />
-            <svg-icon :name="scope.row.deviceType" width="20" height="20" :class="scope.row.deviceStatus+'line'" />
+            <status-badge
+              v-if="scope.row.deviceType === 'ipc'"
+              :status="scope.row.deviceStatus"
+            />
+            <svg-icon
+              :name="scope.row.deviceType"
+              width="20"
+              height="20"
+              :class="scope.row.deviceStatus + 'line'"
+            />
           </div>
           <div class="device-list__device-name">
             <div class="device-list__device-id">{{ scope.row.deviceName }}</div>
@@ -59,7 +71,9 @@
       </el-table-column>
       <el-table-column label="AI启用状态">
         <template slot-scope="scope">
-          <status-badge :status="parseInt(scope.row.status) ? 'on' : 'failed'" />
+          <status-badge
+            :status="parseInt(scope.row.status) ? 'on' : 'failed'"
+          />
           <span>{{ parseInt(scope.row.status) ? '启用' : '停用' }}</span>
         </template>
       </el-table-column>
@@ -69,10 +83,19 @@
           <el-button type="text" @click="rowClick(scope.row)">
             设备详情
           </el-button>
-          <el-button v-permission="['AdminAi']" type="text" @click.stop="startOrStop(scope.row)">
+          <el-button
+            v-permission="['AdminAi']"
+            type="text"
+            @click.stop="startOrStop(scope.row)"
+          >
             {{ parseInt(scope.row.status) ? '停用' : '启用' }}
           </el-button>
-          <el-button v-permission="['AdminAi']" type="text" :disabled="scope.row.status === '1' ? true : false" @click.stop="detach(scope.row)">
+          <el-button
+            v-permission="['AdminAi']"
+            type="text"
+            :disabled="scope.row.status === '1' ? true : false"
+            @click.stop="detach(scope.row)"
+          >
             解绑
           </el-button>
         </template>
@@ -91,10 +114,15 @@
 
 <script lang="ts">
 import { Component, Mixins, Watch } from 'vue-property-decorator'
-import StatusBadge from '@/components/StatusBadge/index.vue'
-import { getAttachedDevice, getAiAlarm } from '@/api/ai-app'
-import { startAppResource, stopAppResource, unBindAppResource } from '@/api/device'
-import AppMixin from '@/views/AI/mixin/app-mixin'
+import StatusBadge from '@vss/base/components/StatusBadge/index.vue'
+// import { getAttachedDevice, getAiAlarm } from '@/api/ai-app'
+import { getAttachedDevice, getAiAlarm } from '@vss/ai/api/index'
+import {
+  startAppResource,
+  stopAppResource,
+  unBindAppResource
+} from '@vss/device/api/device'
+import AppMixin from '@vss/ai/mixin/app-mixin'
 // import { GroupModule } from '@/store/modules/group'
 // import { getGroups } from '@/api/group'
 
@@ -117,13 +145,22 @@ export default class extends Mixins(AppMixin) {
   private periodTypeUpdated(newVal) {
     switch (newVal) {
       case '今天':
-        this.period.period = [new Date().setHours(0, 0, 0, 0), new Date().setHours(23, 59, 59, 999)]
+        this.period.period = [
+          new Date().setHours(0, 0, 0, 0),
+          new Date().setHours(23, 59, 59, 999)
+        ]
         break
       case '近3天':
-        this.period.period = [this.getDateBefore(2), new Date().setHours(23, 59, 59, 999)]
+        this.period.period = [
+          this.getDateBefore(2),
+          new Date().setHours(23, 59, 59, 999)
+        ]
         break
       case '自定义时间':
-        this.period.period = [this.getDateBefore(6), new Date().setHours(0, 0, 0, 0)]
+        this.period.period = [
+          this.getDateBefore(6),
+          new Date().setHours(0, 0, 0, 0)
+        ]
         break
     }
   }
@@ -134,7 +171,7 @@ export default class extends Mixins(AppMixin) {
       // await this.getGroupsList()
       await this.getAttachedDevice()
       this.getAlarms()
-    } catch (e){
+    } catch (e) {
       console.log(e)
     } finally {
       this.loading = false
@@ -147,11 +184,13 @@ export default class extends Mixins(AppMixin) {
   // }
 
   private async getAttachedDevice() {
-    const { deviceList, pageNum, pageSize, totalNum } = await getAttachedDevice({
-      appId: this.$route.query.appid,
-      pageNum: this.pager.pageNum,
-      pageSize: this.pager.pageSize
-    })
+    const { deviceList, pageNum, pageSize, totalNum } = await getAttachedDevice(
+      {
+        appId: this.$route.query.appid,
+        pageNum: this.pager.pageNum,
+        pageSize: this.pager.pageSize
+      }
+    )
     this.devices = deviceList
     this.pager.pageNum = pageNum
     this.pager.pageSize = pageSize
@@ -168,18 +207,10 @@ export default class extends Mixins(AppMixin) {
       handleName: method,
       type: '应用',
       msg: h('div', undefined, [
-        h(
-          'span',
-          undefined,
-          `确定要${method}选中的设备吗？`
-        ),
-        h(
-          'div',
-          { class: 'batch-list' },
-          [h('p', undefined, [
-            h('span', undefined, device.name)
-          ])]
-        )
+        h('span', undefined, `确定要${method}选中的设备吗？`),
+        h('div', { class: 'batch-list' }, [
+          h('p', undefined, [h('span', undefined, device.name)])
+        ])
       ]),
       method: m,
       payload: {
@@ -187,7 +218,7 @@ export default class extends Mixins(AppMixin) {
         appIds: [this.$route.query.appid],
         deviceId: device.deviceId
       },
-      onSuccess: async() => {
+      onSuccess: async () => {
         this.$message.success(`已通知${method}AI应用`)
         await this.getAttachedDevice()
         this.getAlarms()
@@ -204,11 +235,7 @@ export default class extends Mixins(AppMixin) {
       handleName: '解绑',
       type: '应用',
       msg: h('div', undefined, [
-        h(
-          'span',
-          undefined,
-          '确定要在该设备上解绑当前AI应用吗？'
-        )
+        h('span', undefined, '确定要在该设备上解绑当前AI应用吗？')
         // h(
         //   'div',
         //   { class: 'batch-list' },
@@ -251,10 +278,14 @@ export default class extends Mixins(AppMixin) {
     this.loading = true
     this.alarms = []
     this.getAlarm(this.$route.query.appid, null, this.period.period)
-    const promiseArray = this.devices.map(item => this.getAlarm(this.$route.query.appid, item.deviceId, this.period.period))
+    const promiseArray = this.devices.map((item) =>
+      this.getAlarm(this.$route.query.appid, item.deviceId, this.period.period)
+    )
     await Promise.all(promiseArray)
-    this.devices = this.devices.map(device => {
-      const result = this.alarms.filter(alarm => alarm.deviceId === device.deviceId)
+    this.devices = this.devices.map((device) => {
+      const result = this.alarms.filter(
+        (alarm) => alarm.deviceId === device.deviceId
+      )
       return { ...device, count: result[0].count }
     })
     this.loading = false
@@ -262,10 +293,25 @@ export default class extends Mixins(AppMixin) {
 
   public async getAlarm(appId, deviceId, period) {
     if (deviceId) {
-      const res = await getAiAlarm({ appId, deviceId, startTime: period[0], endTime: this.period.periodType === '自定义时间' ? period[1] + this.msOfADay : period[1] })
+      const res = await getAiAlarm({
+        appId,
+        deviceId,
+        startTime: period[0],
+        endTime:
+          this.period.periodType === '自定义时间'
+            ? period[1] + this.msOfADay
+            : period[1]
+      })
       this.alarms.push(res)
     } else {
-      const { count } = await getAiAlarm({ appId, startTime: period[0], endTime: this.period.periodType === '自定义时间' ? period[1] + this.msOfADay : period[1] })
+      const { count } = await getAiAlarm({
+        appId,
+        startTime: period[0],
+        endTime:
+          this.period.periodType === '自定义时间'
+            ? period[1] + this.msOfADay
+            : period[1]
+      })
       this.totalAlarm = count
     }
   }
