4877c3b4ac3dafbc67779203eeed460e016c5dff
龙虾狂魔 <672412250@qq.com>
2023-05-16 17:42:59 +0800

    feat: IAM 设备云台锁定、锁定云端录像Action暂时隐藏，不在策略语法中展示

--- src/settings.ts
+++ src/settings.ts
@@ -140,7 +140,8 @@ const settings: ISettings = {
       actionKey: 'ivs:LockDevicePTZ',
       actionDesc: '拥有设备云台锁定权限，并展示实时预览菜单',
       allowAutoSelected: ['ivs:ControlDevicePTZ'],
-      resourceType: ['directory', 'device', 'channel']
+      resourceType: ['directory', 'device', 'channel'],
+      version: 'v1'
     },
     {
       actionName: '设备预置位配置',
@@ -179,7 +180,8 @@ const settings: ISettings = {
       actionKey: 'ivs:LockCloudRecord',
       actionDesc: '拥有云端录像文件的锁定权限，并展示录像回放菜单',
       allowAutoSelected: ['ivs:GetCloudRecord'],
-      resourceType: ['directory', 'device', 'channel']
+      resourceType: ['directory', 'device', 'channel'],
+      version: 'v1'
     },
     // 视图级联服务(视图级联服务可放入向上级联，作为一种协议进行控制)
     // 柳州上公有云暂时屏蔽（https://dcloud.yuque.com/ctyun/poea5w/kcsd37oaspl3s7gx）
