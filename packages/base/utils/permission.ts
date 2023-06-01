d9de3cb59befbc700a81975dffb245d2017ffd34
xiaoren <xiaoren@chinatelecom.cn>
2023-06-01 14:08:27 +0800

    fix: 业务平台中主账号拥有所有权限

--- packages/base/utils/permission.ts
+++ packages/base/utils/permission.ts
@@ -1,8 +1,15 @@
 import { UserModule } from '@/store/modules/user'
+import { SystemType, AppModule } from '@/store/modules/app'
 
+/**
+ * 权限判断
+ * @param value 需要进行判断的权限字段
+ * @param data 权限对象或者多个权限对象组成的数组
+ * @returns 
+ */
 export const checkPermission = (value: string[], data?: any): boolean => {
-  // 主账号拥有所有权限
-  if (!UserModule.iamUserId) {
+  // 业务平台中主账号拥有所有权限
+  if (AppModule.system === SystemType.SYSTEM_USER && !UserModule.iamUserId) {
     return true
   }
   if (value && value instanceof Array && value.length > 0) {
